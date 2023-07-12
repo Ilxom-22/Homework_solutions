ae381b540b9c06774041353478dd66a3b1dfb03b
Ilxom <ilxomkarimjonov22@gmail.com>
2023-07-12 14:23:14 +0500

    First Task Completed.

--- HT-1/Program.cs
+++ HT-1/Program.cs
@@ -1,11 +1,32 @@
-﻿namespace Hometask
+﻿string name;
+string lastName;
+byte age;
+bool success;
+
+// Name input.
+do
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, I speak C#");
-            Console.WriteLine("Need to pull");
-        }
-    }
-}
+    Console.Write("Input your name: ");
+    name = Console.ReadLine();
+} while (string.IsNullOrWhiteSpace(name));
+
+// Last Name input.
+do
+{
+    Console.Write("Input your last name: ");
+    lastName = Console.ReadLine();
+} while (string.IsNullOrWhiteSpace(lastName));
+
+// Age input.
+do
+{
+    Console.Write("Input your age: ");
+    success = byte.TryParse(Console.ReadLine(), out age);
+} while (!success || age > 120 || age < 12);
+
+
+
+// Printing out the credentials.
+Console.WriteLine(@$"Name: {name}
+Last Name: {lastName}
+Age: {age}");
