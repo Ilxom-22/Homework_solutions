01cbc6825c5def6f503de16d1c023cfc215898bc
Ilxom <ilxomkarimjonov22@gmail.com>
2023-07-12 13:32:45 +0500

    Changed the Program.cs file

--- HT-1/Program.cs
+++ HT-1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Hometask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I speak C#");
+        }
+    }
+}
