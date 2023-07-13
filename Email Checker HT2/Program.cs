6ccf2614a3b41d8bcf90f940c3762c16d8b04a07
Ilxom <ilxomkarimjonov22@gmail.com>
2023-07-13 14:14:46 +0500

    HomeTask 1 and 2 of the 3rd lesson is completed.

---
+++ Email Checker HT2/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace HT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email;
+            int index = default;
+            bool containsCapitalLetter = default;
+
+            do
+            {
+                Console.Write("Input your email please: ");
+                email = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(email));
+
+
+            for (int i = 0; i < email.Length; i++)
+            {
+                if (email[i] != '.' && email[i] < 48)
+                {
+                    index = i;
+                    break;
+                }
+                if (email[i] > '9' && email[i] < '@')
+                {
+                    index = i;
+                    break;
+                }
+                if (email[i] > 'Z' && email[i] < 'a')
+                {
+                    index = i;
+                    break;
+                }
+                if (email[i] > 'z')
+                {
+                    index = i;
+                    break;
+                }
+                if (email[i] >= 'A' && email[i] <= 'Z')
+                {
+                    containsCapitalLetter = true;
+                }
+            }
+
+            if (index == default)
+            {
+                if (containsCapitalLetter)
+                    Console.WriteLine("Recommended version of the email: " + email.ToLower());
+                else
+                    Console.WriteLine("Everything is alright.");
+            }
+            else
+                Console.WriteLine("There is an error in your email: " + email.Substring(index, 1));
+        }
+    }
+}
---
+++ HT_1/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace HT_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+            bool numbersIncluded = Include("Include numbers");
+            bool lettersIncluded = Include("Include letters");
+            bool charactersIncluded = Include("Include characters");
+            byte passwordLength;
+            bool converted;
+
+            do
+            {
+                Console.Write("Input the password length: ");
+                converted = byte.TryParse(Console.ReadLine(), out passwordLength);
+            } while (!converted || passwordLength < 4 || passwordLength > 16);
+
+            if (!numbersIncluded && !lettersIncluded && !charactersIncluded)
+                lettersIncluded = true;
+
+            password = GeneratePassword(numbersIncluded, lettersIncluded, charactersIncluded, passwordLength);
+            Console.WriteLine(password);
+        }
+
+        private static bool Include(string question)
+        {
+            string template = $"{question} [Y/N]: ";
+            string answer;
+
+            do
+            {
+                Console.Write(template);
+                answer = Console.ReadLine().ToLower().Trim();
+            } while (answer != "y" && answer != "n");
+
+
+            return answer == "y";
+        }
+
+        private static string GeneratePassword(bool number, bool letter, bool character, byte length)
+        {
+            string raw = string.Empty;
+            var rd = new Random();
+
+            string numbers = "0123456789";
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            string characters = "!@#$%^&*()-_+=[]{}|\\:;\"'<>,.?/";
+
+            while (raw.Length < length)
+            {
+                if (number)
+                    raw += numbers[rd.Next(numbers.Length)];
+                if (letter)
+                    raw += letters[rd.Next(letters.Length)];
+                if (character)
+                    raw += characters[rd.Next(characters.Length)];
+            }
+
+            raw = raw.Substring(0, length);
+            char[] password = raw.ToCharArray();
+
+            //Fisher-Yates Shuffle Algorithm.
+            for (int i = password.Length - 1; i > 0; i--)
+            {
+                int j = rd.Next(i + 1);
+                (password[i], password[j]) = (password[j], password[i]);
+            }
+
+            return new string(password);
+        }
+
+
+    }
+}
