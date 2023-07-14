3e430896dde0b4c8213b0339f3e3936d0ec94bf0
Ilxom <ilxomkarimjonov22@gmail.com>
2023-07-14 10:42:43 +0500

    Essay Analysis Task Completed

---
+++ Essay Analysis/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Essay_Analysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var essayBall = 100;
+            var essay = "Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
+                       "quaerat est quas commodi quibusdam labore, nihil doloribus quam temporibus inventore optio expedita consectetur " +
+                       "voluptatem QUIDEM nulla soluta earum. Numquam rem alias minima culpa iste distinctio. Eum similique est consequuntur minus, " +
+                       "odio nisi recusandae iure asperiores facere, reiciendis voluptate maiores! Repellat, dolorum!";
+
+            var words = essay.Split();
+            var sentences = essay.Split('.', '?', '!');
+            var containsLargeWords = false;
+            var repeatedWordsExistance = false;
+            var twentyPercent = words.Length * 0.2f;
+            Dictionary<string, int> counter = new Dictionary<string, int>();
+
+
+            if (words.Length < 500)
+                essayBall -= 5;
+
+            
+            
+            foreach (var word in words)
+            {
+                if (word.Length > 20)
+                    containsLargeWords = true;
+
+                if (counter.ContainsKey(word.ToLower()))
+                    counter[word.ToLower()]++;
+                else
+                    counter[word.ToLower()] = 1;
+
+                if (counter[word.ToLower()] > twentyPercent)
+                    repeatedWordsExistance = true;
+            }
+
+            if (containsLargeWords)
+                essayBall -= 20;
+            
+            if (repeatedWordsExistance)
+                essayBall -= 5;
+
+        
+
+            foreach (var sentence in sentences)
+            {
+                if (!string.IsNullOrWhiteSpace(sentence) && !(sentence.Trim()[0] >= 'A' && sentence.Trim()[0] <= 'Z'))
+                    essayBall -= 5;
+
+                var word = sentence.Trim().Split();
+                for (int i = 1; i < word.Length; i++)
+                {
+                    if (word[i] != word[i].ToLower())
+                        essayBall -= 10;
+                }
+            }
+
+            Console.WriteLine(essayBall);
+        }
+
+    }
+}
