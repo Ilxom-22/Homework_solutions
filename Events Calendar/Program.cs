3fb6e7cf6fc29bfa1fdadd00f8fc0a89e0974925
Ilxom <ilxomkarimjonov22@gmail.com>
2023-07-21 15:24:01 +0500

    Events Calendar Task completed.

---
+++ Events Calendar/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+namespace Events;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> eventNames = new()
+        {
+            "TechConnect", "FutureTech Expo 2023", "DataCon", "CyberSecure Summit 2023", "CloudXchange"
+        };
+
+        List<DateTime> eventDates = new()
+        {
+            new(2023, 12, 10, 19, 20, 0),
+            new (2023, 9, 12, 10, 30, 0),
+            new(2024, 1, 15, 22, 0, 0),
+            new(2023, 7, 30, 23, 40, 0),
+            new(2024, 4, 29, 14, 0, 0)
+        };
+
+        SortEvents(eventNames, eventDates);
+
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+        Console.WriteLine("EN");
+        Print(eventNames, eventDates);
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("ru");
+        Console.WriteLine("RU");
+        Print(eventNames, eventDates);
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("uz");
+        Console.WriteLine("UZ");
+        Print(eventNames, eventDates);
+    }
+
+    private static void Print(List<string> eventNames, List<DateTime> eventDates)
+    {
+        for (var i = 0; i < eventNames.Count; i++)
+            Console.WriteLine($"{eventNames[i]} - {eventDates[i]}");
+
+        Console.WriteLine();
+    }
+
+
+    private static void SortEvents(List<string> eventNames, List<DateTime> eventDates)
+    {
+        for (var i = 0; i < eventDates.Count; i++)
+            for (var j = i + 1; j < eventDates.Count; j++)
+                if (eventDates[i] > eventDates[j])
+                {
+                    (eventDates[i], eventDates[j]) = (eventDates[j], eventDates[i]);
+                    (eventNames[i], eventNames[j]) = (eventNames[j], eventNames[i]);
+                }
+    }
+}
---
+++ Valid Email/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Email;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] email = new string[5];
+        string pattern = @"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$";
+        var checker = new Regex(pattern);
+
+        for (var i = 0; i < 5; i++)
+        {
+            Console.Write("Input an email address: ");
+            email[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine();
+        foreach (var i in email)
+            if (checker.IsMatch(i.Trim()))
+                Console.WriteLine($"{i} - Valid!");
+            else
+                Console.WriteLine($"{i} - Invalid!");
+    }
+}
