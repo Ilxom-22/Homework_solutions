c5491646d6f2d5381ed72b9e814a3e2bfcbd7c40
Ilxom <ilxomkarimjonov22@gmail.com>
2023-07-24 16:23:27 +0500

    Password Generator Done

---
+++ Password Generator OOP/PasswordGenerator.cs
@@ -0,0 +1,63 @@
+﻿namespace Password;
+
+public class PasswordGenerator
+{
+    private bool _hasDigits;
+    private bool _hasLetters;
+    private bool _hasSymbols;
+    private byte _length;
+
+
+
+    public PasswordGenerator(bool digits, bool letters, bool symbols, byte length)
+    {
+        _hasDigits = digits;
+        _hasLetters = letters;
+        _hasSymbols = symbols;
+        _length = length;
+
+        if (!digits && !letters && !symbols)
+            _hasLetters = true;
+    }
+
+
+
+    public string GeneratePassword()
+    {
+        string raw = string.Empty;
+        var rd = new Random();
+
+        string numbers = "0123456789";
+        string letters = "abcdefghijklmnopqrstuvwxyz";
+        string characters = "!@#$%^&*()-_+=[]{}|\\:;\"'<>,.?/";
+
+        while (raw.Length < _length)
+        {
+            if (_hasDigits)
+                raw += numbers[rd.Next(numbers.Length)];
+            if (_hasLetters)
+                raw += letters[rd.Next(letters.Length)];
+            if (_hasSymbols)
+                raw += characters[rd.Next(characters.Length)];
+        }
+        return Shuffle(raw, rd);
+    }
+
+
+
+    private string Shuffle(string raw, Random rd)
+    {
+        raw = raw.Substring(0, _length);
+        char[] password = raw.ToCharArray();
+
+
+        //Fisher-Yates Shuffle Algorithm.
+        for (int i = password.Length - 1; i > 0; i--)
+        {
+            int j = rd.Next(i + 1);
+            (password[i], password[j]) = (password[j], password[i]);
+        }
+
+        return new string(password);
+    }
+}
---
+++ Password Generator OOP/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Password;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool numbersIncluded = Include("Include numbers");
+        bool lettersIncluded = Include("Include letters");
+        bool charactersIncluded = Include("Include characters");
+        byte passwordLength;
+
+
+        do
+        {
+            Console.Write("Input the password length: ");
+        } while (!byte.TryParse(Console.ReadLine(), out passwordLength) || passwordLength < 4 || passwordLength > 16);
+
+        var passwordGenerator = new PasswordGenerator(numbersIncluded, lettersIncluded, charactersIncluded, passwordLength);
+
+        string password = passwordGenerator.GeneratePassword();
+        Console.WriteLine(password);
+    }
+
+
+
+    private static bool Include(string question)
+    {
+        string template = $"{question} [Y/N]: ";
+        string answer;
+
+        do
+        {
+            Console.Write(template);
+            answer = Console.ReadLine().ToLower().Trim();
+        } while (answer != "y" && answer != "n");
+
+        return answer == "y";
+    }
+}
