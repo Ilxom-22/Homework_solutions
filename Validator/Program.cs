4cf359642db15a5de8ef7f2902185190528022cb
Ilxom <ilxomkarimjonov22@gmail.com>
2023-07-24 21:37:47 +0500

    Validator Task Completed

---
+++ Validator/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Validation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name;
+        string lastName;
+        string age;
+        string email;
+        string phoneNumber;
+
+        Console.Write("Input your name: ");
+        name = Console.ReadLine();
+
+        Console.Write("Input your lastName: ");
+        lastName = Console.ReadLine();
+
+        Console.Write("Input your age: ");
+        age = Console.ReadLine();
+
+        Console.Write("Input your email: ");
+        email = Console.ReadLine();
+
+        Console.Write("Input your phone number: ");
+        phoneNumber = Console.ReadLine();
+
+        Console.WriteLine("Validation statuses.");
+        Console.WriteLine($"Name: {Validator.FirstName(name)}");
+        Console.WriteLine($"Last Name: {Validator.LastName(lastName)}");
+        Console.WriteLine($"Age: {Validator.Age(age)}");
+        Console.WriteLine($"Email: {Validator.IsEmail(email)}");
+        Console.WriteLine($"Phone number: {Validator.PhoneNumber(phoneNumber)}");
+    }
+}
---
+++ Validator/Validator.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Validation;
+
+public class Validator
+{
+    public static bool Age(string age)
+    {
+        byte realAge;
+
+        if (!byte.TryParse(age, out realAge))
+            return false;
+
+        if (realAge >= 100)
+            return false;
+
+        return true;
+    }
+
+
+
+    public static bool FirstName(string name)
+    {
+        string pattern = @"^(?=.{2,40}$)(?!.*--)[A-Z][a-zA-Z-]*[A-Za-z]$";
+
+        return Regex.IsMatch(name, pattern);
+    }
+
+
+
+    public static bool LastName(string lastName)
+    {
+        string pattern = @"^(?!.*(?:  |-|'| {2,}))[A-Z][A-Za-z-' ]{0,38}[A-Za-z]$";
+
+        return Regex.IsMatch(lastName, pattern);
+    }
+
+
+
+    public static bool IsEmail(string email)
+    {
+        string pattern = @"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$";
+
+        return Regex.IsMatch(email, pattern);
+    }
+
+
+
+    public static bool PhoneNumber(string phone)
+    {
+        string pattern = @"^\+998(90|91|50|93|94|95|97|88|98|77|78|99|33)\d{7}$";
+
+        return Regex.IsMatch(phone, pattern);
+    }
+}
