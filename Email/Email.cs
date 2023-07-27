cd849955187e53aa29be7d81fc52d76feeda628d
Ilxom <ilxomkarimjonov22@gmail.com>
2023-07-27 14:18:13 +0500

    Email Task completed

---
+++ Email/Email.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Email;
+
+public class Email
+{
+    private string _to;
+    private string _from;
+    private string _subject;
+    private string _content;
+
+
+    public Email(string to, string from, string subject, string content)
+    {
+        To = to;
+        From = from;
+        Subject = subject;
+        Content = content;
+    }
+
+
+    public string To
+    {
+        get => _to;
+        set => _to = CheckEmail(value) ? value : throw new ArgumentOutOfRangeException();
+    }
+
+    public string From
+    {
+        get => _from;
+        set => _from = CheckEmail(value) ? value : throw new ArgumentOutOfRangeException();
+    }
+
+    public string Subject
+    {
+        get => _subject;
+        set => _subject = CheckSubject(value) ? value : throw new ArgumentOutOfRangeException();
+    }
+
+    public string Content
+    {
+        get => _content;
+        set => _content = CheckContent(value) ? value : throw new ArgumentOutOfRangeException();
+    }
+
+
+
+    public bool CheckEmail(string email)
+    {
+        string pattern = @"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$";
+        return Regex.IsMatch(email, pattern);
+    }
+
+
+
+    private bool CheckSubject(string subject)
+    {
+        string pattern = @"^[\w\d\s!#$%&'*+\-/=?^_`{|}~]+( [\w\d\s!#$%&'*+\-/=?^_`{|}~]+)*$";
+        return Regex.IsMatch(subject, pattern);
+    }
+
+
+
+    private bool CheckContent(string content)
+    {
+        string pattern = @"^[\w\d\s!#$%&'*+\-/=?^_`{|}~.,:;()\[\]<>]+( [\w\d\s!#$%&'*+\-/=?^_`{|}~.,:;()\[\]<>]+)*$";
+        return Regex.IsMatch(content, pattern);
+    }
+
+
+    public override string ToString()
+    {
+        return @$"To: {_to}
+From: {_from}
+Subject: {_subject}
+Content: {_content}";
+    }
+}
--- Email/Program.cs
+++ Email/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        var validEmail = new Email("james@gmail.com", "ilxom@gmail.com", "The Latest Commit issue", "...");
+        Console.WriteLine(validEmail);
+        Console.WriteLine();
 
+
+        var invalidEmail = new Email("jamesgmail.com", "ilxom@gmailcom", "123", "...");
+        Console.WriteLine(invalidEmail);
     }
 }
