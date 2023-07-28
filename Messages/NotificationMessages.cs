21173cc351ac6d1eaaa00ecca13eff5d547a7092
Ilxom <ilxomkarimjonov22@gmail.com>
2023-07-28 14:45:39 +0500

    Message task completed

---
+++ Messages/NotificationMessages.cs
@@ -0,0 +1,37 @@
+﻿namespace Messages;
+
+public class NotificationMessages
+{
+    private readonly Dictionary<string, string> messages = new();
+
+
+
+    protected KeyValuePair<string, string>? FindMessage(string message)
+    {
+        foreach (var pair in messages)
+            if (pair.Key.Contains(message, StringComparison.OrdinalIgnoreCase))
+                return pair;
+
+        return null;
+    }
+
+
+
+    public string? SearchMessage(string message)
+    {
+        var output = FindMessage(message);
+
+        return output == null ? null : output.Value.Value;
+    }
+
+
+
+    public bool AddMessage(string messageName, string content)
+    {
+        if (messages.ContainsKey(messageName))
+            return false;
+
+        messages[messageName] = content;
+        return true;
+    }
+}
---
+++ Messages/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Messages;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var notification = new NotificationMessages();
+
+        notification.AddMessage("SuccRegistration" , "You successfully registered");
+        notification.AddMessage("AskPassword", "Enter your password");
+        notification.AddMessage("Blocked", "Your account has been blocked");
+
+
+        var searchedMessage = notification.SearchMessage("blocked");
+        Console.WriteLine(searchedMessage == null ? "Not Found!" : searchedMessage);
+
+        searchedMessage = notification.SearchMessage("Hello");
+        Console.WriteLine(searchedMessage == null ? "Message Not Found!" : searchedMessage);
+    }
+}
