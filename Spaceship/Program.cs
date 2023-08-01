c3b2de9c20cba61c93e6c186727b2c680227b4e1
Ilxom <ilxomkarimjonov22@gmail.com>
2023-08-01 10:31:07 +0500

    Spaceship Task Completed.

---
+++ Spaceship/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Space;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var spaceship = new Spaceship()
+        {
+            Name = "Stellar Voyager",
+            Speed = 299,
+            Trajectory = "Journey through the Asteroid Belt." +
+                         "Navigating the treacherous asteroid belt between Mars and Jupiter, Starfire skillfully maneuvers through " +
+                         "the clutter of rocky debris. The onboard AI system dynamically calculates the safest and fastest route, weaving " +
+                         "the spaceship through the gaps between asteroids like a skilled pilot."
+        };
+
+        Console.WriteLine(spaceship);
+        Console.WriteLine();
+
+        spaceship.Speed = 425;
+        spaceship.Trajectory = "Homecoming." +
+                               "With its mission accomplished, Starfire enters Martian orbit once again. " +
+                               "The spaceship's crew, excited to share their groundbreaking findings with the scientific community, " +
+                               "prepares for a smooth docking procedure at the orbiting space station.";
+        Console.WriteLine(spaceship);
+
+
+    }
+}
---
+++ Spaceship/Spaceship.cs
@@ -0,0 +1,21 @@
+﻿namespace Space;
+
+public class Spaceship
+{
+    private string _trajectory;
+    public string Name { get; init; }
+    public int Fuel { get; } = 40;
+    public int Speed { get; set; }
+    public string Trajectory
+    {
+        set => _trajectory = value;
+    }
+
+
+    public override string ToString()
+    {
+        return $@"Name: {Name}
+Fuel: {Fuel}
+Speed: {Speed}";
+    }
+}
