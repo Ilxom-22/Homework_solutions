6896a62ac595fd92816df14b18bbbda154a71fcb
Ilxom <ilxomkarimjonov22@gmail.com>
2023-08-05 17:24:33 +0500

    Validator task completed.

---
+++ Validator static/Program.cs
@@ -0,0 +1,51 @@
+﻿var email = "james@gmail.com";
+var email2 = "james-carter.com";
+
+Validator.IsValidEmail(email, out string validEmail);
+Print(email, validEmail);
+Validator.IsValidEmail(email2, out string validEmail2);
+Print(email2, validEmail2);
+Console.WriteLine();
+
+
+var name = "   James   ";
+var name2 = "James2";
+
+Validator.IsValidName(name, out string validName);
+Print(name, validName);
+Validator.IsValidName(name2, out string validName2);
+Print(name2, validName2);
+Console.WriteLine();
+
+
+var number = "+998937771123";
+var number2 = "+123456789101";
+
+Validator.IsValidPhoneNumber(number, out string validNumber);
+Print(number, validNumber);
+Validator.IsValidPhoneNumber(number2, out string validNumber2);
+Print(number2, validNumber2);
+Console.WriteLine();
+
+
+var age = 33;
+var age1 = 255;
+
+if(Validator.IsValidAge(age))
+    Console.WriteLine($"{age} - Valid!");
+else
+    Console.WriteLine($"{age} - Invalid!");
+
+
+if (Validator.IsValidAge(age1))
+    Console.WriteLine($"{age1} - Valid!");
+else
+    Console.WriteLine($"{age1} - Invalid!");
+
+void Print(string test, string valid)
+{
+    if (valid != null)
+        Console.WriteLine($"{valid} - Valid!");
+    else
+        Console.WriteLine($"{test} - Invalid!");
+}
---
+++ Validator static/Validator.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.RegularExpressions;
+
+public static class Validator
+{
+    public static bool IsValidName(string name, out string formattedName)
+    {
+        var checker = new Regex("^[A-Za-z'-]{1,50}$");
+        formattedName = name.Trim();
+
+        if (checker.IsMatch(name))
+            return true;
+
+        
+        if (checker.IsMatch(formattedName))
+            return true;
+
+        formattedName = null;
+        return false;
+    }
+
+
+
+    public static bool IsValidEmail(string email, out string formattedEmail)
+    {
+        var checker = new Regex(@"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$");
+        formattedEmail = email.Trim();
+
+        if (checker.IsMatch(email))
+            return true;
+
+        if (checker.IsMatch(formattedEmail))
+            return true;
+
+        formattedEmail = null;
+        return false;
+    }
+
+
+
+    public static bool IsValidAge(in int age)
+    {
+        if (age < 0 || age > 120)
+            return false;
+
+        return true;
+    }
+
+
+
+    public static bool IsValidPhoneNumber(in string phoneNumber, out string formattedPhoneNumber)
+    {
+        var checker = new Regex(@"^\+998(90|91|50|93|94|95|97|88|98|77|78|99|33)\d{7}$");
+        formattedPhoneNumber = phoneNumber.Trim();
+
+        if (checker.IsMatch(phoneNumber))
+            return true;
+
+        if (checker.IsMatch(formattedPhoneNumber))
+            return true;
+
+        formattedPhoneNumber = null;
+        return false;
+    }
+}
