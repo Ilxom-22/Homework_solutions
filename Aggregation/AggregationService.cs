967e95b31cfd05a09d19673620347ca9daac7709
Ilxom <ilxomkarimjonov22@gmail.com>
2023-08-05 17:50:57 +0500

    Aggregation Service Task completed.

---
+++ Aggregation/AggregationService.cs
@@ -0,0 +1,63 @@
+﻿public static class AggregationService
+{
+    public static int Sum(params int[] values)
+    {
+        int sum = 0;
+        foreach (var value in values)
+            sum += value;
+
+        return sum;
+    }
+
+
+
+    public static double Average(params int[] values)
+    {
+        return Math.Round((double)Sum(values) / values.Length, 2);
+    }
+
+
+
+
+    public static int Min(params int[] values)
+    {
+        var min = int.MaxValue;
+
+        foreach (var value in values)
+            if (value < min)
+                min = value;
+
+        return min;
+    }
+
+
+
+
+    public static int Max(params int[] values)
+    {
+        var max = int.MinValue;
+
+        foreach (var value in values)
+            if (value > max)
+                max = value;
+
+        return max;
+    }
+
+
+
+
+    public static void Increment(ref int value)
+    {
+        if (value < int.MaxValue)
+            value++;
+    }
+
+
+
+    public static void Decrement(ref int value)
+    {
+        if (value > int.MinValue)
+            value--;
+    }
+}
---
+++ Aggregation/Program.cs
@@ -0,0 +1,27 @@
+﻿var array = new int[] { 12, 22, 55, 7, 20 };
+Console.WriteLine($"Sum: {AggregationService.Sum(array)}");
+Console.WriteLine($"Average: {AggregationService.Average(array)}");
+Console.WriteLine($"Min: {AggregationService.Min(array)}");
+Console.WriteLine($"Max: {AggregationService.Max(array)}");
+
+
+var n1 = 6;
+var n2 = int.MaxValue;
+var n3 = -10;
+var n4 = int.MinValue;
+
+Console.Write($"Increment -> {n1} = ");
+AggregationService.Increment(ref n1);
+Console.WriteLine(n1);
+
+Console.Write($"Increment -> {n2} = ");
+AggregationService.Increment(ref n2);
+Console.WriteLine(n2);
+
+Console.Write($"Decrement -> {n3} = ");
+AggregationService.Decrement(ref n3);
+Console.WriteLine(n3);
+
+Console.Write($"Decrement -> {n4} = ");
+AggregationService.Decrement(ref n4);
+Console.WriteLine(n4);
