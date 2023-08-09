83a6fa0ff8242afcc29a730eb91f413262e8af43
Ilxom <ilxomkarimjonov22@gmail.com>
2023-08-09 17:44:22 +0500

    Updated online market

--- Online Market/Program.cs
+++ Online Market/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿var paymentService = UzumPaymentProvider.GetInstance(new MilliyHumo("9700871335299087", "Agrobank"), 2);
+var card1 = new MilliyHumo("6400138743890906", "Aloqabank");
+
+var market = OnlineMarket.GetInstance(paymentService, card1);
+
+var product1 = new Product("CyberGuard Pro VPN Router", 199.90m);
+var product2 = new Product("DataSaver Plus External Hard Drive - 2TB", 129.95m);
+var product3 = new Product("PixelView UltraWide Curved Monitor - 34", 499m);
+var product4 = new Product("CodeMaster Pro Mechanical Keyboard", 89.99m);
+var product5 = new Product("NetBlaze Secure Antivirus Software - 1 Year Subscription", 49.99m);
+
+market.Add(product1, 5);
+market.Add(product2);
+market.Add(product3, 10);
+market.Add(product4, 7);
+market.Add(product5);
