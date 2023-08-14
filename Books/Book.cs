89716b0532e3d80d4b7dda0d4a8db2f0b196a2e2
Ilxom <ilxomkarimjonov22@gmail.com>
2023-08-14 13:28:32 +0500

    Books Task Completed.

---
+++ Books/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace Books;
+
+internal class Book
+{
+    public Book(string name, string author, byte rating)
+    {
+        Name = name;
+        Author = author;
+        Rating = rating;
+    }
+
+    private byte _rating;
+    public string Name { get; set; }
+    public byte Rating { 
+        get => _rating; 
+        set => _rating = (value is > 0 and <= 10) 
+            ? value 
+            : throw new ArgumentOutOfRangeException(nameof(Rating), "Rating must be within 1 and 10 stars!"); 
+    }
+    public string Author { get; set; }
+}
+
---
+++ Books/Program.cs
@@ -0,0 +1,21 @@
+﻿using Books;
+
+List<Book> books = new List<Book>()
+{
+    new ("Whispers of the Lost Realm", "Emily Turner", 8),
+    new ("Echoes in the Mist", "Michael Greene", 9),
+    new ("Serenade for the Stars", "Jennifer Mitchell", 5),
+    new ("Crimson Secrets", "Emily Turner", 6),
+    new ("Waves of Destiny", "Michael Greene", 10),
+    new ("Silent Whispers", "Jennifer Mitchell", 7),
+    new ("Eternal Skies", "Emily Turner", 4),
+    new ("Forgotten Realms", "Michael Greene", 3),
+    new ("Melodies of Midnight", "Jennifer Mitchell", 2),
+    new ("Abyss of Illusions", "Emily Turner", 1)
+};
+
+
+var emilyBooks = books.Where(book => book.Author == "Emily Turner").OrderByDescending(book => book.Rating).ToList();
+
+Console.WriteLine($"Best Book of {emilyBooks[0].Author} - {emilyBooks[0].Name} - {emilyBooks[0].Rating}");
+Console.WriteLine($"Worst Book of {emilyBooks[^1].Author} - {emilyBooks[^1].Name} - {emilyBooks[^1].Rating}");
