fc9077f99ad61390fb4c1f924621862d69965fa5
Ilxom <ilxomkarimjonov22@gmail.com>
2023-08-14 10:35:38 +0500

    User Task completed.

---
+++ User Model Linq/User.cs
@@ -0,0 +1,17 @@
+﻿namespace User_Model_Linq;
+
+internal class User
+{
+    // Vazifa maqsadi Linq bo'lganligi sababli hech qanday validatsiya qo'shilmadi.
+    public User(string name, string lastName, string email)
+    {
+        Name = name;
+        LastName = lastName;
+        EmailAddress = email;
+    }
+
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+}
+
