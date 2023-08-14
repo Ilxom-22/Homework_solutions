6164fda9f94266bf7721dc231b69aadc0ddd3ca2
Ilxom <ilxomkarimjonov22@gmail.com>
2023-08-14 10:16:35 +0500

    Product Task Completed.

---
+++ Products_Rating/Product.cs
@@ -0,0 +1,35 @@
+﻿namespace Products_Rating;
+
+public class Product
+{
+    public Product(string name, byte stars, int inventory)
+    {
+        Name = name;
+        Stars = stars;
+        Inventory = inventory;
+    }
+
+    private string _name;
+    private byte _stars;
+    private int _inventory;
+
+    public string Name { 
+        get => _name; 
+        set => _name = (!string.IsNullOrWhiteSpace(value)) 
+            ? value 
+            : throw new ArgumentNullException(nameof(Name), "Name can't be null!"); 
+    }
+    public byte Stars {
+        get => _stars; 
+        set => _stars = (value is > 0 and <= 5) 
+            ? value 
+            : throw new ArgumentOutOfRangeException(nameof(Stars), "Rating must be within 1 and 5 stars!"); 
+    }
+    public int Inventory { 
+        get => _inventory; 
+        set => _inventory = (value >= 0) 
+            ? value 
+            : throw new ArgumentOutOfRangeException(nameof(Inventory), "Inventory can't be negative number!"); 
+    }
+}
+
---
+++ Products_Rating/Program.cs
@@ -0,0 +1,24 @@
+﻿using Products_Rating;
+
+var products = new List<Product>
+{
+    new Product("iPhone 12", 4, 60),
+    new Product("Samsung Galaxy S21", 4, 75),
+    new Product("Google Pixel 5", 4, 50),
+    new Product("OnePlus 9 Pro", 4, 60),
+    new Product("Xiaomi Mi 11", 4, 80),
+    new Product("Sony Xperia 1 III", 3, 40),
+    new Product("LG Wing", 3, 30),
+    new Product("Motorola Edge+", 3, 35),
+    new Product("Iphone 14", 5, 10),
+    new Product("Asus ROG Phone 5", 4, 70)
+};
+
+
+products
+    .OrderByDescending(pro => pro.Stars)
+    .ThenByDescending(pro => pro.Inventory)
+    .Take(5)
+    .Select(pro => new {pro.Name, pro.Inventory})
+    .ToList()
+    .ForEach(pro => Console.WriteLine($"{pro.Name} - {pro.Inventory} in stock."));
