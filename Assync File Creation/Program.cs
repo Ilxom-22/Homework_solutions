0c1c543d88ad49ae44158c5148c922d41181d446
Ilxom <ilxomkarimjonov22@gmail.com>
2023-08-23 14:14:49 +0500

    Async File Creation Task Completed.

---
+++ Assync File Creation/Program.cs
@@ -0,0 +1,30 @@
+﻿var usernames = new List<string>()
+{
+    "CrystalPineapple87",
+    "QuantumJester",
+    "MidnightStarlight23",
+    "ElectricWanderer",
+    "SereneNebula42"
+};
+
+var tasks = new List<Task<FileStream?>>();
+var newFolder = "Storage";
+Directory.CreateDirectory(newFolder);
+
+
+foreach (var username in usernames)
+{
+    tasks.Add(Task.Run(() =>
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), newFolder, $"{username.ToLower()}.txt");
+        var stream = File.Create(path);
+        Console.WriteLine($"File for {username} created!");
+        return stream;
+    }));
+
+}
+
+var result = await Task.WhenAll(tasks);
+
+Console.WriteLine("All Tasks Done!");
+
