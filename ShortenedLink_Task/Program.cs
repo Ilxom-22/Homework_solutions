b8cde59182b77b042140f616f115486bc5503e34
Ilxom <ilxomkarimjonov22@gmail.com>
2023-08-31 21:28:32 +0500

    Shortened Link Task completed

---
+++ ShortenedLink_Task/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Custom_type_and_members;
+
+List<ShortenedLink> shortenedLinks = new List<ShortenedLink>
+{
+    new ShortenedLink("https://www.example.com/article123", "https://short.ly/abc123"),
+    new ShortenedLink("https://www.example.net/page456", "https://short.ly/def456"),
+    new ShortenedLink("https://www.sample.org/product789", "https://short.ly/ghi789"),
+    new ShortenedLink("https://www.demo-site.com/video123", "https://short.ly/jkl123"),
+    new ShortenedLink("https://www.test-site.org/blog987", "https://short.ly/mno987")
+};
+
+Console.WriteLine(shortenedLinks[0] == shortenedLinks[1]);
---
+++ ShortenedLink_Task/ShortenedLink.cs
@@ -0,0 +1,33 @@
+﻿namespace Custom_type_and_members;
+
+public class ShortenedLink
+{
+    public ShortenedLink(string url, string shortenedUrl)
+    {
+        Id = Guid.NewGuid();
+        OriginalUrl = url;
+        ShortenedUrl = shortenedUrl;
+    }
+
+    public Guid Id { get; set; }
+    public string OriginalUrl { get; set; }
+    public string ShortenedUrl { get; set; }
+
+
+    public override int GetHashCode()
+    {
+        int hash = 17;
+        hash = hash * 23 + Id.GetHashCode();
+        hash = hash * 23 + OriginalUrl.GetHashCode();
+        hash = hash * 23 + ShortenedUrl.GetHashCode();
+        return hash;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is ShortenedLink)
+            return GetHashCode() == obj.GetHashCode();
+
+        return false;
+    }
+}
