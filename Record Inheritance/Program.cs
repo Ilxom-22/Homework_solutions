48f4a96a7e2af9dd629d258fc716395585fd0197
Ilxom <ilxomkarimjonov22@gmail.com>
2023-09-13 15:17:13 +0500

    Record inheritance task completed

---
+++ Record Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿var manager = new Manager("Mike", "Nickson", 1, "HR", 2000);
+var employee = new Employee("Nick", "Johnson", 2, "HR", 1, 1000);
+
+Console.WriteLine(manager);
+Console.WriteLine(employee);
+
+
+public abstract record Person(string FirstName, string LastName);
+
+public record Manager(string FirstName, string LastName, long Id, string Department, double Salary) : Person(FirstName, LastName);
+
+public record Employee(string FirstName, string LastName, long Id, string Department, long ManagerId, double Salary) : Person(FirstName, LastName);
