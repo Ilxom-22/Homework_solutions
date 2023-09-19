174f13c1f7511a38249150195947f87f0c955426
Ilxom <ilxomkarimjonov22@gmail.com>
2023-09-19 14:05:05 +0500

    Thread safe Queue task completed

---
+++ Thread Safe Queue/CustomQueue.cs
@@ -0,0 +1,32 @@
+﻿public class CustomQueue<T>
+{
+    private readonly Queue<T> _items;
+    private readonly object _locker = new object();
+
+    public CustomQueue()
+        => _items = new Queue<T>();
+
+    public CustomQueue(IEnumerable<T> items) : this()
+    {
+        foreach (var item in items)
+            _items.Enqueue(item);
+    }
+
+    public ValueTask EnqueueAsync(T item)
+    {
+        lock (_locker)
+            _items.Enqueue(item);
+
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask<T> DequeueAnync()
+    {
+        T removedItem;
+        lock (_locker)
+        {
+            removedItem = _items.Dequeue();
+        }
+        return new ValueTask<T>(removedItem);
+    }
+}
---
+++ Thread Safe Queue/Program.cs
@@ -0,0 +1,31 @@
+﻿var a = new Queue<int>();
+
+var customQueue = new CustomQueue<int>();
+
+
+var tasks = new List<Task>()
+{
+    new(() => customQueue.EnqueueAsync(5).AsTask()),
+    new(() => customQueue.EnqueueAsync(4).AsTask()),
+    new(() => customQueue.EnqueueAsync(15).AsTask()),
+    new(() => customQueue.EnqueueAsync(11).AsTask()),
+    new(() => customQueue.EnqueueAsync(51).AsTask()),
+    new(() => customQueue.EnqueueAsync(71).AsTask()),
+    new(() => customQueue.EnqueueAsync(18).AsTask()),
+    new(() => customQueue.EnqueueAsync(45).AsTask()),
+    new(() => customQueue.EnqueueAsync(76).AsTask()),
+    new(() => customQueue.EnqueueAsync(90).AsTask()),
+    new(() => customQueue.EnqueueAsync(12).AsTask()),
+    new (() => customQueue.DequeueAnync().AsTask()),
+    new (() => customQueue.DequeueAnync().AsTask()),
+    new (() => customQueue.DequeueAnync().AsTask()),
+    new (() => customQueue.DequeueAnync().AsTask()),
+    new (() => customQueue.DequeueAnync().AsTask()),
+    new (() => customQueue.DequeueAnync().AsTask()),
+    new (() => customQueue.DequeueAnync().AsTask()),
+    new (() => customQueue.DequeueAnync().AsTask()),
+    new (() => customQueue.DequeueAnync().AsTask()),
+};
+
+Parallel.ForEach(tasks, (task) => task.Start());
+await Task.WhenAll(tasks);
