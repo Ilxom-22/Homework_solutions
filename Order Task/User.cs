72845c523cd369005d5e10b49a1b9a272dc41bdc
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-01 20:37:50 +0500

    Create user model

---
+++ Order Task/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Order_Task;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+
+    public User(string firstName)
+    {
+        Id = Guid.NewGuid();
+        FirstName = firstName;
+    }
+}
