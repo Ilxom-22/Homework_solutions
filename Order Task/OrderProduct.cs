18e7e04eb6d194475a24eb56fcbe78c158d3a000
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-01 20:38:36 +0500

    Create order product many to many connection model

---
+++ Order Task/OrderProduct.cs
@@ -0,0 +1,15 @@
+﻿namespace Order_Task;
+
+public class OrderProduct
+{
+    public Guid Id { get; set; }
+    public Guid ProductId { get; set; }
+    public Guid OrderId { get; set; }
+
+    public OrderProduct(Guid productId, Guid orderId)
+    {
+        Id = Guid.NewGuid();
+        ProductId = productId;
+        OrderId = orderId;
+    }
+}
