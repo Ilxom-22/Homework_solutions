e714105718013649ecafee5647952366a7e037c3
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-01 07:41:22 +0500

    Performance Task Completed

---
+++ Performance Report Task/Program.cs
@@ -0,0 +1,12 @@
+﻿var employeeService = new EmployeeService();
+
+var newEmployee = new Employee("James", "Carter", true);
+
+employeeService.Add(newEmployee);
+
+var accountService = new AccountService(employeeService);
+var performanceService = new PerformanceService(employeeService);
+
+await accountService.CreateReportAsync(newEmployee.Id);
+
+await performanceService.ReportPerformanceAsync(newEmployee.Id);
