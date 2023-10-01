131abdf8cce911cfa5e7aadba0eaf46b75a52b76
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-01 20:38:03 +0500

    Create product model

---
+++ Order Task/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Order_Task;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public Product(string name, decimal price)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Price = price;
+    }
+}
