567afb680c280cbd2a0ab9d621b9a743e50697e6
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-01 16:56:40 +0500

    Linq execution task completed

---
+++ Linq Executions/Program.cs
@@ -0,0 +1,18 @@
+﻿var numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+// Deferred
+var query = numbers.Where(n => n % 2 == 0); 
+
+foreach (var num in query)
+    Console.WriteLine(num);
+
+
+// Immediate
+var immediateQuery = numbers.Where(n => n % 2 == 0).ToList();
+
+// Mixed
+
+var mixedQuery = numbers
+    .Where(n => n % 2 == 0)  // Deferred execution, filtering is deferred
+    .Select(n => n * 2)      // Immediate execution, projection is executed immediately
+    .ToList();                  // Immediate execution
