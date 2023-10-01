b9cadb232e9d977f824f3e46dad9e135d7a6812d
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-01 07:40:44 +0500

    Implement accont service

---
+++ Performance Report Task/AccountService.cs
@@ -0,0 +1,32 @@
+﻿
+public class AccountService
+{
+    private readonly IEmployeeService _employeeService;
+    private const string _folder = "Employees Data";
+
+    public AccountService(IEmployeeService employeeService)
+    {
+        _employeeService = employeeService;
+        EnsureDirectoryExists();
+    }
+
+    public async Task CreateReportAsync(long id)
+    {
+        await Task.Run(() => _employeeService.GetById(id))
+            .ContinueWith(employee => CreateFile(employee.Result));
+    }
+
+    private void CreateFile(Employee employee)
+    {
+        var mutex = new Mutex(false, "OpenFile");
+        
+        var path = Path.Combine(_folder, $"{employee.FirstName} {employee.LastName}.txt");
+
+        mutex.WaitOne();
+        File.WriteAllText(path, $"ID: {employee.Id}\nIsActive: {employee.IsActive}");
+        mutex.ReleaseMutex();
+    }
+
+    private void EnsureDirectoryExists()
+        => Directory.CreateDirectory(_folder);
+}
