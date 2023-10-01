5626ee5a712630c814ee672dd840104ad36d868d
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-01 07:41:03 +0500

    Implement Performance Service

---
+++ Performance Report Task/PerformanceService.cs
@@ -0,0 +1,31 @@
+﻿public class PerformanceService
+{
+    private readonly IEmployeeService _employeeService;
+    private const string _folder = "Employees Data";
+
+    public PerformanceService(IEmployeeService employeeService)
+    {
+        _employeeService = employeeService;
+    }
+
+    public async Task ReportPerformanceAsync(long id)
+    {
+        await Task.Run(() => _employeeService.GetById(id))
+            .ContinueWith(employee => WriteReport(employee.Result));
+    }
+
+    private void WriteReport(Employee employee)
+    {
+        var mutex = new Mutex(false, "OpenFile");
+
+        var path = Path.Combine(_folder, $"{employee.FirstName} {employee.LastName}.txt");
+
+        mutex.WaitOne();
+
+        var data = File.ReadAllText(path);
+        data += "\n\nAll Good :)";
+        File.WriteAllText(path, data);
+
+        mutex.ReleaseMutex();
+    }
+}
