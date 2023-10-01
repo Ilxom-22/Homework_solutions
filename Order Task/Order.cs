ccac776ea7db193ae7436f5ecfc633149dbd9c1f
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-01 20:38:12 +0500

    Create order model

---
+++ Order Task/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace Order_Task;
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public int Amount { get; set; }
+    public Guid UserId { get; set; }
+
+    public Order(int amount, Guid userId)
+    {
+        Id = Guid.NewGuid();
+        Amount = amount;
+        UserId = userId;
+    }
+}
