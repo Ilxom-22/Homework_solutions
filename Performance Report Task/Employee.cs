46665ddf0e2deb68ea29dbad521cf8b77f2646a4
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-01 07:39:24 +0500

    Create Employee model

---
+++ Performance Report Task/Employee.cs
@@ -0,0 +1,25 @@
+﻿public class Employee
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool IsActive { get; set; }
+
+    public Employee(string firstName, string lastName, bool isActive)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        IsActive = isActive;
+    }
+
+    public override int GetHashCode()
+        => HashCode.Combine(FirstName, LastName, IsActive);
+
+    public override bool Equals(object? obj)
+    {
+        if (obj != null && obj is Employee)
+            return GetHashCode() == obj.GetHashCode();
+
+        return false;
+    }
+}
