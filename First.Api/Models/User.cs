2d819170b81daf2949e55d88c193a0e4834dbc17
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-02 12:36:48 +0500

    Create models

---
+++ First.Api/Models/User.cs
@@ -0,0 +1,21 @@
+using FileBaseContext.Abstractions.Models.Entity;
+
+namespace First.Api.Models;
+
+public class User : IFileSetEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public string Password { get; set; }
+
+    public User(string firstName, string lastName, string emailAddress, string password)
+    {
+        Id = Guid.NewGuid();
+        FirstName = firstName;
+        LastName = lastName;
+        EmailAddress = emailAddress;
+        Password = password;
+    }
+}
