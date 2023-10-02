0a712952ba7171566b52073d7cc78a26ee3a2bb9
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-02 12:37:27 +0500

    Create appFileContext

---
+++ First.Api/DataAccess/IDataContext.cs
@@ -0,0 +1,11 @@
+﻿using FileBaseContext.Abstractions.Models.FileSet;
+using First.Api.Models;
+
+namespace First.Api.DataAccess;
+
+public interface IDataContext
+{
+    IFileSet<User, Guid> Users { get; }
+    IFileSet<Order, Guid> Orders { get; }
+    ValueTask SaveChangesAsync();
+}
