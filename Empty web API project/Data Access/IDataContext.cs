323b729cebc7c00b20a0b7eab17011083cba3b64
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-07 14:21:55 +0500

    Create app file context

---
+++ Empty web API project/Data Access/IDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Empty_web_API_project.Models;
+using FileBaseContext.Abstractions.Models.FileSet;
+
+
+namespace Empty_web_API_project.DataAccess;
+
+public interface IDataContext
+{
+    IFileSet<User, Guid> Users { get; }
+    IFileSet<Order, Guid> Orders { get; }
+    ValueTask SaveChangesAsync();
+}
