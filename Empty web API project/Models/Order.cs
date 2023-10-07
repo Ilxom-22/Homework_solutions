49080d61b7c5b3df48f69f1d3a7506f28baa0467
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-07 14:21:15 +0500

    Create models

---
+++ Empty web API project/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using FileBaseContext.Abstractions.Models.Entity;
+
+namespace Empty_web_API_project.Models;
+
+public class Order : IFileSetEntity<Guid>
+{
+    public Guid Id { get; set; }    
+    public int Amount { get; set; }
+    public Guid UserId { get; set; }
+
+    public Order(int amount, Guid userId)
+    {
+        Id = Guid.NewGuid();
+        Amount = amount;
+        UserId = userId;
+    }
+}
