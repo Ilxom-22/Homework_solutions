3a62a3f1831d013047de4f78b848023c8d755334
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-07 14:22:06 +0500

    Add controllers

---
+++ Empty web API project/Controllers/OrdersController.cs
@@ -0,0 +1,53 @@
+﻿using Empty_web_API_project.Models;
+using Empty_web_API_project.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Empty_web_API_project.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+    private readonly IEntityBase<Order> _orderService;
+
+    public OrdersController(IEntityBase<Order> orderService)
+    {   
+        _orderService = orderService;
+    }
+
+    [HttpGet]
+    public IActionResult GetOrders()
+    {
+        return Ok(_orderService.Get(order => true));
+    }
+
+    [HttpGet("{orderId:guid}")]
+    public async ValueTask<IActionResult> GetOrder(Guid id)
+    {
+        return Ok(await _orderService.GetByIdAsync(id));
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> AddOrder(Order order)
+    {
+        return Ok(await _orderService.CreateAsync(order));  
+    }
+
+    [HttpPut]
+    public async ValueTask<IActionResult> UpdateOrder(Order order)
+    {
+        return Ok(await _orderService.UpdateAsync(order));
+    }
+
+    [HttpDelete]
+    public async ValueTask<IActionResult> DeleteOrder(Order order)
+    {
+        return Ok(await _orderService.DeleteAsync(order));
+    }
+
+    [HttpDelete("{orderId:guid}")]
+    public async ValueTask<IActionResult> DeleteOrder(Guid id)
+    {
+        return Ok(await _orderService.DeleteAsync(id));
+    }
+}
---
+++ Empty web API project/Controllers/UserOrdersController.cs
@@ -0,0 +1,22 @@
+﻿using Empty_web_API_project.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Empty_web_API_project.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserOrdersController : ControllerBase
+{
+    private readonly UserOrdersService _userOrdersService;
+
+    public UserOrdersController(UserOrdersService userOrdersService)
+    {
+        _userOrdersService = userOrdersService;
+    }
+
+    [HttpGet("api/orders/by-user/:userId")]
+    public IActionResult Get(Guid id)
+    {
+        return Ok(_userOrdersService.Get(id));
+    }
+}
