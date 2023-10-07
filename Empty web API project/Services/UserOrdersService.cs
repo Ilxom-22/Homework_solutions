ce09c23d4e621e261f119645ca806e6678e38d89
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-07 14:21:34 +0500

    Create services

---
+++ Empty web API project/Services/UserOrdersService.cs
@@ -0,0 +1,18 @@
+﻿using Empty_web_API_project.Models;
+
+namespace Empty_web_API_project.Services;
+
+public class UserOrdersService
+{
+    private readonly IEntityBase<Order> _orderService;
+
+    public UserOrdersService(IEntityBase<Order> orderService)
+    {
+        _orderService = orderService;
+    }
+
+    public IEnumerable<Order> Get(Guid userId)
+    {
+        return _orderService.Get(order => order.UserId == userId);
+    }
+}
