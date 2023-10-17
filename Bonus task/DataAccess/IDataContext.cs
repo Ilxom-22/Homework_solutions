78872fc05494ef3a3f97adb829d407f0bc79e377
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-17 17:26:46 +0500

    Create app file context

---
+++ Bonus task/DataAccess/IDataContext.cs
@@ -0,0 +1,13 @@
+﻿using FileBaseContext.Abstractions.Models.FileSet;
+using Bonus_task.Models;
+
+
+namespace Bonus_Task.DataAccess;
+
+public interface IDataContext
+{
+    IFileSet<User, Guid> Users { get; }
+    IFileSet<Order, Guid> Orders { get; }
+    IFileSet<Bonus, Guid> Bonuses { get; }
+    ValueTask SaveChangesAsync();
+}
