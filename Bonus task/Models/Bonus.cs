9f878510292c953d510f47f693db0d7d11ba2862
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-17 17:26:17 +0500

    Create entities

---
+++ Bonus task/Models/Bonus.cs
@@ -0,0 +1,17 @@
+﻿using FileBaseContext.Abstractions.Models.Entity;
+
+namespace Bonus_task.Models;
+
+public class Bonus : IFileSetEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public double Amount { get; set; }
+    public Guid UserId { get; set; }
+
+    public Bonus(double lastAmount, Guid userId)
+    {
+        Id = Guid.NewGuid();
+        Amount = lastAmount;
+        UserId = userId;
+    }
+}
---
+++ Bonus task/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using FileBaseContext.Abstractions.Models.Entity;
+
+namespace Bonus_task.Models;
+
+public class Order : IFileSetEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public double Amount { get; set; }
+    public Guid UserId { get; set; }
+
+    public Order(double amount, Guid userId)
+    {
+        Id = Guid.NewGuid();
+        Amount = amount;
+        UserId = userId;
+    }
+}
