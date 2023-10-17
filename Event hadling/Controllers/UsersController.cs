cd57ff8095908f4ab98fda7d83f73b5c437098ef
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-17 09:39:26 +0500

    Add endpoints and configure program.cs

---
+++ Event hadling/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Event_hadling.Models;
+using Event_hadling.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Event_hadling.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class UsersController : ControllerBase
+{
+    private readonly UserService _userService;
+    private readonly AccountService _accountService;
+
+    public UsersController(UserService userService, AccountService accountService)
+    {
+        _userService = userService;
+        _accountService = accountService;
+    }
+
+    [HttpGet]
+    public IActionResult GetUsers()
+        => Ok(_userService.GetAllUsers());
+
+    [HttpPost]
+    public IActionResult AddUser(User user)
+        => Ok(_accountService.CreateUser(user));
+}
