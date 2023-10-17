229a5ba83eb4e01829da00a3cedf2d15a5fbc59b
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-17 17:28:53 +0500

    Add endpoints

---
+++ Bonus task/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿using Bonus_task.CompositionServices;
+using Bonus_task.Interfaces;
+using Bonus_task.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bonus_task.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class OrdersController : ControllerBase
+{
+    private readonly IEntityBaseService<Order> _orderService;
+    private readonly UserOrdersService _userOrdersService;
+
+    public OrdersController(IEntityBaseService<Order> orderService, UserOrdersService userOrdersService)
+    {
+        _orderService = orderService;
+        _userOrdersService = userOrdersService;
+    }
+
+    [HttpGet("orders/:userId")]
+    public IActionResult GetUserOrders(Guid userId)
+        => Ok(_userOrdersService.Get(userId));
+
+    [HttpPost]
+    public IActionResult AddOrder(Order order)
+        => Ok(_orderService.CreateAsync(order));
+}
---
+++ Bonus task/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Bonus_task.Interfaces;
+using Bonus_task.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bonus_task.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class UsersController : ControllerBase
+{
+    private readonly IEntityBaseService<User> _userService;
+
+    public UsersController(IEntityBaseService<User> userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public IActionResult GetAllUsers()
+        => Ok(_userService.Get(user => true));
+
+    [HttpGet(":userId")]
+    public async ValueTask<IActionResult> GetUser(Guid userId)
+        => Ok(await _userService.GetByIdAsync(userId));
+
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateUser(User user)
+        => Ok(await _userService.CreateAsync(user));
+}
