774c137e5c256cda1243f4c56b2bcccb102b0c17
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-17 17:27:53 +0500

    Create events

---
+++ Bonus task/Events/BonusEventStore.cs
@@ -0,0 +1,14 @@
+﻿using Bonus_task.Models;
+
+namespace Bonus_task.Events;
+
+public class BonusEventStore
+{
+    public event Func<Bonus, ValueTask>? OnBonusAchieved;
+
+    public async ValueTask CreateBonusAchievedEvent(Bonus bonus)
+    {
+        if (OnBonusAchieved != null)
+            await OnBonusAchieved(bonus);
+    }
+}
---
+++ Bonus task/Events/OrderEventStore.cs
@@ -0,0 +1,14 @@
+﻿using Bonus_task.Models;
+
+namespace Bonus_task.Events;
+
+public class OrderEventStore
+{
+    public event Func<Order, ValueTask>? OnOrderCreated;
+
+    public async ValueTask CreateOrderCreatedEvent(Order order)
+    {
+        if (OnOrderCreated != null)
+            await OnOrderCreated(order);
+    }
+}
