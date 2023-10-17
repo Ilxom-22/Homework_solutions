a1528467a9235467ea9ddb9a1eac3c1e3a29843a
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-17 09:38:49 +0500

    Create necessary models

---
+++ Event hadling/Models/Email.cs
@@ -0,0 +1,15 @@
+﻿namespace Event_hadling.Models;
+
+public class Email
+{
+    public Guid Id { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+
+    public Email(string subject, string body)
+    {
+        Id = Guid.NewGuid();
+        Subject = subject;
+        Body = body;
+    }
+}
---
+++ Event hadling/Models/UsersEventStore.cs
@@ -0,0 +1,12 @@
+﻿namespace Event_hadling.Models;
+
+public class UsersEventStore
+{
+    public event Func<User, ValueTask>? OnUserAdded;
+
+    public async ValueTask CreateUserAddedEvent(User user)
+    {
+        if (OnUserAdded != null)
+            await OnUserAdded(user);
+    }
+}
