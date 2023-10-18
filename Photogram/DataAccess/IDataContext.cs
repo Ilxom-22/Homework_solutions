a65faae83e8c9cc829e372502ed7a7f6362f27fe
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-19 02:01:01 +0500

    Add Data context

---
+++ Photogram/DataAccess/IDataContext.cs
@@ -0,0 +1,11 @@
+﻿using FileBaseContext.Abstractions.Models.FileSet;
+using Photogram.Entities;
+
+namespace Photogram.DataAccess;
+
+public interface IDataContext
+{
+    IFileSet<User, Guid> Users { get; }
+    IFileSet<Post, Guid> Posts { get; }
+    ValueTask SaveChangesAsync();
+}
