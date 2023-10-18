106eddc92ba4e4733457224a6717b22a6ee57c30
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-19 02:00:47 +0500

    Create necessary services

---
+++ Photogram/Services/PostService.cs
@@ -0,0 +1,43 @@
+﻿using Photogram.Entities;
+using Phtotogram.Interfaces;
+using System.Linq.Expressions;
+
+namespace Photogram.Services;
+
+public class PostService : IEntityBaseService<Post>
+{
+    public ValueTask<Post> CreateAsync(Post entity, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<Post> Get(Expression<Func<Post, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<ICollection<Post>> GetAsync(IEnumerable<Guid> ids, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<Post> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<Post> UpdateAsync(Post entity, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<Post> DeleteAsync(Guid id, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<Post> DeleteAsync(Post entity, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Photogram/Services/UserService.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq.Expressions;
+using Photogram.Entities;
+using Photogram.DataAccess;
+using Phtotogram.Interfaces;
+
+namespace Photogram.Services;
+
+public class UserService : IEntityBaseService<User>
+{
+    private readonly IDataContext _dataContext;
+
+    public UserService(IDataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    public IQueryable<User> Get(Expression<Func<User, bool>> predicate)
+        => _dataContext.Users.Where(predicate.Compile()).AsQueryable();
+    
+    public ValueTask<ICollection<User>> GetAsync(IEnumerable<Guid> ids, CancellationToken cancellationToken = default)
+        => new ValueTask<ICollection<User>>(Get(user => ids.Contains(user.Id)).ToList());
+    
+    public ValueTask<User> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        var user = _dataContext.Users.FirstOrDefault(user => user.Id == id);
+
+        if (user == null)
+            throw new ArgumentException("User not found!");
+
+        return new ValueTask<User>(user);
+    }
+
+    public async ValueTask<User> CreateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        await _dataContext.Users.AddAsync(user, cancellationToken);
+
+        if (saveChanges) await _dataContext.SaveChangesAsync();
+
+        return user;
+    }
+
+    public async ValueTask<User> UpdateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        var foundUser = await GetByIdAsync(user.Id, cancellationToken);
+
+        foundUser.FirstName = user.FirstName;
+        foundUser.LastName = user.LastName;
+        foundUser.EmailAddress = user.EmailAddress;
+
+        await _dataContext.SaveChangesAsync();
+
+        return foundUser;
+    }
+
+    public async ValueTask<User> DeleteAsync(Guid id, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        var foundUser = await GetByIdAsync(id, cancellationToken);
+
+        if (foundUser is null)
+            throw new InvalidOperationException("User not found");
+
+        await _dataContext.Users.RemoveAsync(foundUser, cancellationToken);
+
+        if (saveChanges) await _dataContext.SaveChangesAsync();
+
+        return foundUser;
+    }
+
+    public async ValueTask<User> DeleteAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
+        => await DeleteAsync(user.Id, saveChanges, cancellationToken);
+}
