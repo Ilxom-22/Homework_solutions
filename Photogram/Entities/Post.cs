36e417f456900b832388d9f673dee3020e3b39ca
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-19 02:00:35 +0500

    Create necessary models

---
+++ Photogram/Entities/Post.cs
@@ -0,0 +1,21 @@
+﻿using FileBaseContext.Abstractions.Models.Entity;
+
+namespace Photogram.Entities;
+
+public class Post : IFileSetEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string ImagePath { get; set; }
+    public Guid AuthorId { get; set; }
+
+    public Post(string title, string content, string imagePath, Guid authorId)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Content = content;
+        ImagePath = imagePath;
+        AuthorId = authorId;
+    }
+}
---
+++ Photogram/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using FileBaseContext.Abstractions.Models.Entity;
+
+namespace Photogram.Entities;
+
+public class User : IFileSetEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public string Password { get; set; }
+    public string ProfileImagePath { get; set; }
+
+    public User(string firstName, string lastName, string emailAddress, string password, string profileImagePath)
+    {
+        Id = Guid.NewGuid();
+        FirstName = firstName;
+        LastName = lastName;
+        EmailAddress = emailAddress;
+        Password = password;
+        ProfileImagePath = profileImagePath;
+    }
+}
