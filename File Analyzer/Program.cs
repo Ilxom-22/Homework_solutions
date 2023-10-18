5c89864b760a20846caf73ec7fde2aa90d09df9b
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-18 14:22:12 +0500

    Test file and folder analyzer service

---
+++ File Analyzer/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using File_Analyzer;
+
+var path = @"D:\C# Homework\Homework_solutions\File Analyzer\bin\Debug\net7.0";
+var fileService = new FileAndFolderAnalyzerService();
+
+Console.WriteLine($"Directories count: {fileService.GetFoldersCount(path)}");
+Console.WriteLine($"Files count: {fileService.GetFilesCount(path)}");
+Console.WriteLine($"Files Size: {fileService.GetFilesSizeSum(path)} KB");
+Console.WriteLine($"Biggest File's full path: {fileService.GetTheBiggestFilePath(path)}");
