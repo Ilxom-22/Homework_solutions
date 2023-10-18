47e453f3a3ea77fdcc2785dcd8b5a82ffc88e58f
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-19 01:58:40 +0500

    Test user folders management project

---
+++ User folders/Program.cs
@@ -0,0 +1,15 @@
+﻿using User_folders.SeedData;
+using User_folders.Services;
+
+var users = SeedData.GetUsers().ToList();
+
+var directoryService = new DirectoryService();
+var fileService = new FileService();
+var cleanUpService = new CleanUpService(directoryService, fileService);
+
+directoryService.CreateUserFolders(users);
+
+for (var i = 0; i < users.Count; ++i)
+    fileService.CreateFilesForUser(directoryService.GetUserFolderPath(users[i]));
+
+cleanUpService.CleanUp();
