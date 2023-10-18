32c4a27cff8683c2d3003de2708ad13c0c87f5d5
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-19 01:58:00 +0500

    Create file service

---
+++ User folders/Services/FileService.cs
@@ -0,0 +1,32 @@
+﻿using Bogus;
+
+namespace User_folders.Services;
+
+public class FileService
+{
+    public void CreateFilesForUser(string userFolderPath)
+    {
+        GenerateFile(Path.Combine(userFolderPath, "Resume"), "Resume");
+        GenerateFile(Path.Combine(userFolderPath, "Profile"), "Profile");
+    }
+
+    public void DeleteFile(string filePath)
+    {
+        File.Delete(filePath);
+    }
+
+    private void GenerateFile(string path, string fileName)
+    {
+        var faker = new Faker();
+        var random = new Random();
+
+        var filePath = Path.Combine(path, fileName + faker.PickRandom(SeedData.SeedData.GetFileExtensions()));
+
+        var fileStream = File.Create(filePath);
+
+        var writer = new StreamWriter(fileStream);
+        writer.WriteLine(faker.Lorem.Paragraphs(random.Next(1, 1000)));
+        writer.Flush();
+        writer.Close();
+    }
+}
