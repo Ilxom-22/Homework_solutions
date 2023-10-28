9d9bed614f3860b9e17a4e485ed9f75c72fff8b8
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-28 11:40:57 +0500

    Add Auth service and controller

---
+++ Authorization.Api/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Authorization.Api.Models.Identity;
+using Authorization.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly AuthService _authService;
+
+    public AuthController(AuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async ValueTask<IActionResult> RegisterAsync(RegistrationDetails registrationDetails)
+        => Ok(await _authService.RegisterAsync(registrationDetails));
+
+    [HttpPost("login")]
+    public async ValueTask<IActionResult> LoginAsync(LoginDetails loginDetails)
+        => Ok(await _authService.LoginAsync(loginDetails));
+}
---
+++ Authorization.Api/Services/AuthService.cs
@@ -0,0 +1,43 @@
+﻿using Authorization.Api.Models.Entities;
+using Authorization.Api.Models.Identity;
+using System.Security.Authentication;
+
+namespace Authorization.Api.Services;
+
+public class AuthService
+{
+    private readonly TokenGeneratorService _tokenGeneratorService;
+    private static readonly List<User> _users = new List<User>();
+
+    public AuthService(TokenGeneratorService tokenGeneratorService)
+        => _tokenGeneratorService = tokenGeneratorService;
+
+    public ValueTask<bool> RegisterAsync(RegistrationDetails registrationDetails)
+    {
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            FirstName = registrationDetails.FirstName,
+            LastName = registrationDetails.LastName,
+            EmailAddress = registrationDetails.EmailAddress,
+            Password = registrationDetails.Password,
+            PhoneNumber = registrationDetails.PhoneNumber
+        };
+
+        _users.Add(user);
+
+        return new ValueTask<bool>(true);
+    }
+
+    public ValueTask<string> LoginAsync(LoginDetails loginDetails)
+    {
+        var foundUser = _users.Find(user => user.EmailAddress == loginDetails.EmailAddress && user.Password == loginDetails.Password);
+
+        if (foundUser == null)
+            throw new AuthenticationException("Login details are invalid!");
+
+        var accessToken = _tokenGeneratorService.GetToken(foundUser);
+
+        return new ValueTask<string>(accessToken);
+    }
+}
