8589120f55fbb1ebe314ab11d60cd4580774e4d3
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-28 11:40:08 +0500

    Add Jwt Settings

---
+++ Authorization.Api/Models/Settings/JwtSettings.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable CS8618
+
+namespace Authorization.Api.Models.Settings;
+
+public class JwtSettings
+{
+    public string SecretKey { get; set; }
+
+    public bool ValidateAudience { get; set; }
+
+    public string ValidAudience { get; set; }
+
+    public bool ValidateIssuer { get; set; }
+
+    public string ValidIssuer { get; set; }
+
+    public bool ValidateLifetime { get; set; }
+
+    public bool ValidateIssuerSigningKey { get; set; }
+
+    public int ExpirationTimeInMinutes { get; set; }
+}
