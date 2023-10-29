2966ccbffac61ac92d933680f12ca8d0a1542551
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-29 13:44:49 +0500

    Add data context

---
+++ File Upload/DataAccess/AppFileContext.cs
@@ -0,0 +1,15 @@
+﻿using File_Upload.Models.Entities;
+using FileBaseContext.Abstractions.Models.FileSet;
+using FileBaseContext.Context.Models.Configurations;
+using FileBaseContext.Context.Models.FileContext;
+
+namespace File_Upload.DataAccess;
+
+public class AppFileContext : FileContext, IDataContext
+{
+    public IFileSet<User, Guid> Users => Set<User, Guid>(nameof(Users));    
+
+    public IFileSet<StorageFile, Guid> StorageFiles => Set<StorageFile, Guid>(nameof(StorageFiles));
+
+    public AppFileContext(IFileContextOptions<AppFileContext> fileContextOptions) : base(fileContextOptions) { }
+}
---
+++ File Upload/DataAccess/IDataContext.cs
@@ -0,0 +1,12 @@
+﻿using File_Upload.Models.Entities;
+using FileBaseContext.Abstractions.Models.FileSet;
+
+namespace File_Upload.DataAccess;
+
+public interface IDataContext
+{
+    IFileSet<User, Guid> Users { get; }
+    IFileSet<StorageFile, Guid> StorageFiles { get; }
+
+    ValueTask SaveChangesAsync();
+}
