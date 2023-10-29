153b9c31a927b299cec247463142f0d646c3fd92
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-29 13:45:52 +0500

    Add controllers

---
+++ File Upload/Controller/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using File_Upload.Models.Identity;
+using File_Upload.Services.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace File_Upload.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly AuthService _authService;
+
+    public AuthController(AuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async ValueTask<IActionResult> RegisterAsync(RegisterDetails registrationDetails)
+        => Ok(await _authService.RegisterAsync(registrationDetails));
+
+    [HttpPost("login")]
+    public async ValueTask<IActionResult> LoginAsync(LoginDetails loginDetails)
+        => Ok(await _authService.LoginAsync(loginDetails));
+}
---
+++ File Upload/Controller/FilesController.cs
@@ -0,0 +1,27 @@
+﻿using File_Upload.Models.Constants;
+using File_Upload.Services.Processing;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace File_Upload.Controller;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class FilesController : ControllerBase
+{
+    private readonly FileProcessingService _fileProcessingService;
+
+    public FilesController(FileProcessingService fileProcessingService)
+    {
+        _fileProcessingService = fileProcessingService;
+    }
+
+    [HttpPost("upload")]
+    public async ValueTask<IActionResult> UploadFileAsync(IFormFile file)
+    {
+        var userId = User.Claims.First(claim => claim.Type == ClaimConstants.UserId).Value;
+
+        return Ok(await _fileProcessingService.UploadFileAsync(file, Guid.Parse(userId)));
+    }
+}
