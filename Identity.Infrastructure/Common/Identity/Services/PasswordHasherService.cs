8875bac5ca1d770c7eaec473c124ce84a2bccf07
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-30 12:13:28 +0500

    Add Password hasher service

---
+++ Identity.Infrastructure/Common/Identity/Services/PasswordHasherService.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Application.Common.Identity.Services;
+using BC = BCrypt.Net.BCrypt;
+
+namespace Identity.Infrastructure.Common.Identity.Services;
+
+public class PasswordHasherService : IPasswordHasherService
+{
+    public string HashPassword(string password)
+        => BC.HashPassword(password);
+
+    public bool VerifyPassword(string password, string passwordHash)
+        => BC.Verify(password, passwordHash);
+}
