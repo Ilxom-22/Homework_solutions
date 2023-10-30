4fe91f5d2d79a488211ac08030bc1d6fdd5a14ea
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-30 12:13:12 +0500

    Create Email sender service

---
+++ Identity.Application/Common/Notification/Services/EmailSenderService.cs
@@ -0,0 +1,6 @@
+﻿namespace Identity.Application.Common.Notification.Services;
+
+public interface IEmailSenderService
+{
+    ValueTask<bool> SendEmailAsync(string emailAddress, string message);
+}
---
+++ Identity.Infrastructure/Common/Notification/Services/EmailSenderService.cs
@@ -0,0 +1,30 @@
+﻿using Identity.Application.Common.Notification.Services;
+using Identity.Application.Common.Settings;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+
+namespace Identity.Infrastructure.Common.Notification.Services;
+
+public class EmailSenderService : IEmailSenderService
+{
+    private readonly EmailSenderSettings _senderSettings;
+
+    public EmailSenderService(IOptions<EmailSenderSettings> senderSettings)
+        => _senderSettings = senderSettings.Value;
+
+    public async ValueTask<bool> SendEmailAsync(string emailAddress, string message)
+    {
+        var mail = new MailMessage(_senderSettings.CredentialAddress, emailAddress);
+        mail.Subject = "Successful Registration";
+        mail.Body = message;
+
+        var client = new SmtpClient(_senderSettings.Host, _senderSettings.Port);
+        client.Credentials = new NetworkCredential(_senderSettings.CredentialAddress, _senderSettings.Password);
+        client.EnableSsl = true;
+
+        await client.SendMailAsync(mail);
+
+        return true;
+    }
+}
