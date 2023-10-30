87943f774c1d2820dffc9c2a7996c7d9af3b5625
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-30 12:10:42 +0500

    Create data context

---
+++ Identity.Persistence/DataContext/AppFileContext.cs
@@ -0,0 +1,15 @@
+﻿using FileBaseContext.Abstractions.Models.FileSet;
+using FileBaseContext.Context.Models.Configurations;
+using FileBaseContext.Context.Models.FileContext;
+using Identity.Domain.Entities;
+
+namespace Identity.Persistence.DataContext;
+
+public class AppFileContext : FileContext, IDataContext
+{
+    public IFileSet<User, Guid> Users => Set<User, Guid>(nameof(Users));
+
+    public AppFileContext(IFileContextOptions<AppFileContext> fileContextOptions) : base(fileContextOptions)
+    {
+    }
+}
---
+++ Identity.Persistence/DataContext/IDataContext.cs
@@ -0,0 +1,11 @@
+﻿using FileBaseContext.Abstractions.Models.FileSet;
+using Identity.Domain.Entities;
+
+namespace Identity.Persistence.DataContext;
+
+public interface IDataContext
+{
+    IFileSet<User, Guid> Users { get; }
+
+    ValueTask SaveChangesAsync();
+}
