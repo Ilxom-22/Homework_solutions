7f2b6b40c6b91c0ca41463dbab48824ff12cd176
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-30 12:12:44 +0500

    Create user service

---
+++ Identity.Infrastructure/Foundations/UserService.cs
@@ -0,0 +1,44 @@
+﻿using Identity.Application.Foundations;
+using Identity.Domain.Entities;
+using Identity.Persistence.DataContext;
+using System.Linq.Expressions;
+
+namespace Identity.Infrastructure.Foundations;
+
+public class UserService : IEntityBaseService<User>
+{
+    private readonly IDataContext _appFileContext;
+
+    public UserService(IDataContext context)
+        => _appFileContext = context;
+
+    public IEnumerable<User> Get(Expression<Func<User, bool>> predicate)
+        => _appFileContext.Users.Where(predicate.Compile()).AsQueryable();
+
+    public async ValueTask<User> GetByIdAsync(Guid id)
+        => await _appFileContext.Users.FindAsync(id) ?? throw new ArgumentOutOfRangeException(nameof(id), "User not found!");
+
+    public async ValueTask<User> CreateAsync(User user)
+    {
+        if (!IsValidUser(user))
+            throw new ArgumentException("Invalid user");
+
+        if (!IsUnique(user))
+            throw new ArgumentException("Username or email address already exists!");
+
+        await _appFileContext.Users.AddAsync(user);
+        await _appFileContext.SaveChangesAsync();
+
+        return user;
+    }
+
+    private bool IsValidUser(User user)
+        => !string.IsNullOrWhiteSpace(user.Username)
+            && !string.IsNullOrWhiteSpace(user.FirstName)
+            && !string.IsNullOrWhiteSpace(user.LastName)
+            && !string.IsNullOrWhiteSpace(user.EmailAddress)
+            && !string.IsNullOrWhiteSpace(user.PasswordHash);
+
+    private bool IsUnique(User user)
+        => !_appFileContext.Users.Any(self => self.Username == user.Username || self.EmailAddress == user.EmailAddress);
+}
