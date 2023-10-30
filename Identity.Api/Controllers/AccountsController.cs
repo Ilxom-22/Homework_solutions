d09b9bd3ac3423c023eb3777c67e8ffafa70c11d
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-30 12:15:01 +0500

    Add controllers

---
+++ Identity.Api/Controllers/AccountsController.cs
@@ -0,0 +1,18 @@
+﻿using Identity.Application.Common.Identity.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountsController : ControllerBase
+{
+    private readonly IAccountService _accountService;
+
+    public AccountsController(IAccountService accountService)
+        => _accountService = accountService;
+
+    [HttpPut("verification/{token}")]
+    public async ValueTask<IActionResult> VerificateAsync([FromRoute] string token)
+        => Ok(await _accountService.VerificateAsync(token));
+}
---
+++ Identity.Api/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Identity.Application.Common.Identity.Models;
+using Identity.Application.Common.Identity.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+        => _authService = authService;
+
+    [HttpPost("register")]
+    public async ValueTask<IActionResult> RegisterAsync([FromBody] RegistrationDetails registrationDetails)
+        => Ok(await _authService.RegisterAsync(registrationDetails));
+
+    [HttpPost("login")]
+    public async ValueTask<IActionResult> LoginAsync([FromBody] LoginDetails loginDetails)
+        => Ok(await _authService.LoginAsync(loginDetails));
+}
