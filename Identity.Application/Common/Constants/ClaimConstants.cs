37664ba9dcc967b017762a85ccf0f324d326f346
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-30 12:11:25 +0500

    Create constant and identity models

---
+++ Identity.Application/Common/Constants/ClaimConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.Application.Common.Constants;
+
+public static class ClaimConstants
+{
+    public const string UserId = "UserId";
+
+    public const string Username = "Username";
+}
