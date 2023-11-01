bcd4c393b3e6890e70f2f3385924867a490d1333
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-01 14:06:26 +0500

    Add foundations services

---
+++ Library.Infrastructure/Common/Foundations/AuthorService.cs
@@ -0,0 +1,60 @@
+﻿using Library.Application.Common.Entity;
+using Library.Domain.Entities;
+using Library.Persistence.DataContexts;
+using System.Linq.Expressions;
+
+namespace Library.Infrastructure.Common.Foundations;
+
+public class AuthorService : IEntityBaseService<Author>
+{
+    private readonly AppDbContext _appDbContext;
+
+    public AuthorService(AppDbContext context)
+    {
+        _appDbContext = context;        
+    }
+
+    public IQueryable<Author> Get(Expression<Func<Author, bool>>? predicate = null)
+        => predicate != null ? _appDbContext.Authors.Where(predicate) : _appDbContext.Authors;
+
+    public ValueTask<ICollection<Author>> GetAsync(IEnumerable<Guid> ids, CancellationToken cancellationToken = default)
+        => new(Get(author => ids.Contains(author.Id)).ToList());
+
+    public async ValueTask<Author> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        => await _appDbContext.Authors.FindAsync(id, cancellationToken) ?? throw new ArgumentException("Author not found!");
+
+    public async ValueTask<Author> CreateAsync(Author author, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        await _appDbContext.Authors.AddAsync(author, cancellationToken);
+
+        await _appDbContext.SaveChangesAsync();
+
+        return author;
+    }
+
+    public async ValueTask<Author> UpdateAsync(Author author, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        var foundAuthor = await GetByIdAsync(author.Id, cancellationToken);
+
+        foundAuthor.FirstName = author.FirstName;
+        foundAuthor.LastName = author.LastName;
+
+        _appDbContext.Authors.Update(foundAuthor);
+
+        return foundAuthor;
+    }
+
+    public async ValueTask<Author> DeleteAsync(Guid id, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        var foundAuthor = await GetByIdAsync(id, cancellationToken);
+
+        _appDbContext.Authors.Remove(foundAuthor);
+
+        if (saveChanges) await _appDbContext.SaveChangesAsync();
+
+        return foundAuthor;
+    }
+
+    public async ValueTask<Author> DeleteAsync(Author author, bool saveChanges = true, CancellationToken cancellationToken = default)
+        => await DeleteAsync(author.Id, saveChanges, cancellationToken);
+}
---
+++ Library.Infrastructure/Common/Foundations/BookService.cs
@@ -0,0 +1,63 @@
+﻿using Library.Application.Common.Entity;
+using Library.Domain.Entities;
+using Library.Persistence.DataContexts;
+using System.Linq.Expressions;
+
+namespace Library.Infrastructure.Common.Foundations;
+
+public class BookService : IEntityBaseService<Book>
+{
+    private readonly AppDbContext _appDbContext;
+
+    public BookService(AppDbContext context)
+    {
+        _appDbContext = context;
+    }
+
+    public IQueryable<Book> Get(Expression<Func<Book, bool>>? predicate = null)
+       => predicate != null ? _appDbContext.Books.Where(predicate) : _appDbContext.Books;
+
+    public ValueTask<ICollection<Book>> GetAsync(IEnumerable<Guid> ids, CancellationToken cancellationToken = default)
+        => new(Get(book => ids.Contains(book.Id)).ToList());
+
+    public async ValueTask<Book> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        => await _appDbContext.Books.FindAsync(id, cancellationToken) ?? throw new ArgumentException("Book not found");
+
+    public async ValueTask<Book> CreateAsync(Book book, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        await _appDbContext.Books.AddAsync(book, cancellationToken);
+
+        if (saveChanges) await _appDbContext.SaveChangesAsync(cancellationToken);
+
+        return book;
+    }
+
+    public async ValueTask<Book> UpdateAsync(Book book, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        var foundBook = await GetByIdAsync(book.Id, cancellationToken);
+
+        foundBook.AuthorId = book.AuthorId;
+        foundBook.Description = book.Description;
+        foundBook.Title = book.Title;
+
+        _appDbContext.Books.Update(foundBook);
+
+        if (saveChanges) await _appDbContext.SaveChangesAsync();
+
+        return foundBook;
+    }
+
+    public async ValueTask<Book> DeleteAsync(Guid id, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        var foundBook = await GetByIdAsync(id, cancellationToken);
+
+        _appDbContext.Books.Remove(foundBook);
+
+        if (saveChanges) await _appDbContext.SaveChangesAsync();
+
+        return foundBook;
+    }
+
+    public async ValueTask<Book> DeleteAsync(Book book, bool saveChanges = true, CancellationToken cancellationToken = default)
+     => await DeleteAsync(book.Id, saveChanges, cancellationToken);
+}
