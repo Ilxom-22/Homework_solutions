2e124cca003953450392758981d4b36b51d7b681
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-01 14:05:52 +0500

    Add db context and migrations

---
+++ Library.Persistence/DataContexts/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Persistence.DataContexts;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Book> Books => Set<Book>();
+
+    public DbSet<Author> Authors => Set<Author>();
+
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+}
