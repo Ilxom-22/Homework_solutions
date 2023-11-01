5d206058d39c7faf3f457d26ad45ba6ab99a8e9f
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-01 14:06:36 +0500

    Add controllers

---
+++ Library.Api/Controllers/AuthorsController.cs
@@ -0,0 +1,39 @@
+﻿using Library.Application.Common.Entity;
+using Library.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthorsController : ControllerBase
+{
+    private readonly IEntityBaseService<Author> _authorService;
+
+    public AuthorsController(IEntityBaseService<Author> authorService)
+        => _authorService = authorService;
+
+    [HttpGet]
+    public IActionResult GetAllAuthors()
+    {
+        var result = _authorService.Get(author => true);
+
+        return result.Any() ? Ok(result) : NoContent();    
+    }
+
+    [HttpGet("{authorId}")]
+    public async ValueTask<IActionResult> GetAuthorById([FromRoute] Guid authorId)
+        => Ok(await _authorService.GetByIdAsync(authorId));
+
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateAuthor([FromBody] Author author)
+        => Ok(await _authorService.CreateAsync(author));
+
+    [HttpPut]
+    public async ValueTask<IActionResult> UpdateAuthor([FromBody] Author author)
+        => Ok(await _authorService.UpdateAsync(author));
+
+    [HttpDelete("{authorId}")]
+    public async ValueTask<IActionResult> DeleteAuthorById([FromRoute] Guid authorId)
+        => Ok(await _authorService.DeleteAsync(authorId));
+}
---
+++ Library.Api/Controllers/BooksController.cs
@@ -0,0 +1,39 @@
+﻿using Library.Application.Common.Entity;
+using Library.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly IEntityBaseService<Book> _bookService;
+
+    public BooksController(IEntityBaseService<Book> bookService)
+        => _bookService = bookService;
+
+    [HttpGet]
+    public IActionResult GetAllBooks()
+    {
+        var result = _bookService.Get(book => true);
+
+        return result.Any() ? Ok(result) : NoContent();
+    }
+
+    [HttpGet("{bookId}")]
+    public async ValueTask<IActionResult> GetBookById([FromRoute] Guid bookId)
+        => Ok(await _bookService.GetByIdAsync(bookId));
+
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateBook([FromBody] Book book)
+        => Ok(await _bookService.CreateAsync(book));
+
+    [HttpPut]
+    public async ValueTask<IActionResult> UpdateBook([FromBody] Book book)
+        => Ok(await _bookService.UpdateAsync(book));
+
+    [HttpDelete("{bookId}")]
+    public async ValueTask<IActionResult> DeleteBookById([FromRoute] Guid bookId)
+        => Ok(await _bookService.DeleteAsync(bookId));
+}
