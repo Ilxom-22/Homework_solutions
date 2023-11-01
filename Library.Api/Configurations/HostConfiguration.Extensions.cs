1f470ab81e8d5204a4c31d1ae6eae611c8f9ffad
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-01 14:06:46 +0500

    Add host configurations

---
+++ Library.Api/Configurations/HostConfiguration.Extensions.cs
@@ -0,0 +1,60 @@
+﻿using Library.Application.Common.Entity;
+using Library.Domain.Entities;
+using Library.Infrastructure.Common.Foundations;
+using Library.Persistence.DataContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    public static WebApplicationBuilder AddDevTools(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
+    {
+        builder.Services
+            .AddScoped<IEntityBaseService<Author>, AuthorService>()
+            .AddScoped<IEntityBaseService<Book>, BookService>();
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddPersistence(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnectionString"));
+        });
+
+        return builder;
+    }
+
+    public static WebApplication UseDevTools(this WebApplication app) 
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    }
+
+    public static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+}
---
+++ Library.Api/Configurations/HostConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace Library.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder
+            .AddDevTools()
+            .AddExposers()
+            .AddPersistence()
+            .AddServices();
+
+        return new(builder);
+    }
+
+    public static ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        app
+            .UseDevTools()
+            .UseExposers();
+
+        return new(app);
+    }
+}
