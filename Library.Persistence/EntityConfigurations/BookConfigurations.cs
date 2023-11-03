34036f5e8351f7a4c110c695d36801079e74a796
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-03 17:19:49 +0500

    Enable model configurations in db

---
+++ Library.Persistence/EntityConfigurations/BookConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Library.Persistence.EntityConfigurations;
+
+public class BookConfigurations : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.HasOne<Author>().WithMany().HasForeignKey(book => book.AuthorId);
+    }
+}
