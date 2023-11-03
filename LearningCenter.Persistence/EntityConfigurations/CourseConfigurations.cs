d06ebc0fe9374be325636b21da0ddfd7c4e3f713
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-03 17:17:52 +0500

    Create course related configurations

---
+++ LearningCenter.Persistence/EntityConfigurations/CourseConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using LearningCenter.Domain.Entities.Courses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearningCenter.Persistence.EntityConfigurations;
+
+public class CourseConfigurations : IEntityTypeConfiguration<Course>
+{
+    public void Configure(EntityTypeBuilder<Course> builder)
+    {
+        builder.HasOne(course => course.Teacher)
+            .WithMany(user => user.TeacherCourses)
+            .HasForeignKey(course => course.TeacherId);
+    }
+}
---
+++ LearningCenter.Persistence/EntityConfigurations/StudentCourseConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using LearningCenter.Domain.Entities.Courses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearningCenter.Persistence.EntityConfigurations;
+
+public class StudentCourseConfigurations : IEntityTypeConfiguration<CourseStudent>
+{
+    public void Configure(EntityTypeBuilder<CourseStudent> builder)
+    {
+        builder.HasKey(x => new {x.CourseId, x.StudentId});
+
+        builder.HasOne(x => x.Course).WithMany(i => i.CourseStudents).HasForeignKey(x => x.CourseId);
+
+        builder.HasOne(x => x.Course).WithMany(i => i.CourseStudents).HasForeignKey(x => x.StudentId);  
+    }
+}
