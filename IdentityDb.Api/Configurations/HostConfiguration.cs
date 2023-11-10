fdb9ee376469fd51b75dc3f6bb9054297b1bf67f
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-10 11:15:21 +0500

    Create host configurations

---
+++ IdentityDb.Api/Configurations/HostConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace IdentityDb.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder
+            .AddPersistence()
+            .AddDevTools()
+            .AddExposers();
+
+        return new(builder);
+    }
+
+    public static ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        app
+            .UseDevTools()
+            .UseExposers();
+
+        return new(app);
+    }
+}
