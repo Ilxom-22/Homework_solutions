b76974ab3a32b3a862992e66e4f236eeae5829db
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-11 12:40:28 +0500

    install necessary packages in api and configure host configurations

---
+++ BlogSite.Api/Configurations/HostConfiguration.Extensions.cs
@@ -0,0 +1,35 @@
+﻿namespace BlogSite.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    public static WebApplicationBuilder AddDevTools(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+        return builder;
+    }
+
+    public static WebApplication UseDevTools(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    }
+
+    public static WebApplication UseExposers(this WebApplication app) 
+    {
+        app.MapControllers();
+
+        return app;
+    }
+}
