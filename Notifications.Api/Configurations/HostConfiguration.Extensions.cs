7ccd5369c918c32a27fdb2f2b57525bef193cc8b
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-14 08:44:22 +0500

    Set up minimum host configurations

---
+++ Notifications.Api/Configurations/HostConfiguration.Extensions.cs
@@ -0,0 +1,35 @@
+﻿namespace Notifications.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    private static WebApplicationBuilder AddDevTools(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+        return builder;
+    }
+
+    private static WebApplication UseDevTools(this WebApplication app) 
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    }
+
+    private static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+}
