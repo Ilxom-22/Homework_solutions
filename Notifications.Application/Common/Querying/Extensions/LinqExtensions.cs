b1339b5da51fd8f9808852f55728b157d06bd5c9
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-14 10:26:26 +0500

    Add filtering models and linq extensions

---
+++ Notifications.Application/Common/Querying/Extensions/LinqExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Notifications.Application.Common.Querying.Models;
+
+namespace Notifications.Application.Common.Querying.Extensions;
+
+public static class LinqExtensions
+{
+    public static IEnumerable<TSource> ApplyPagination<TSource>(this IEnumerable<TSource> source, FilterPagination pagination) => 
+        source.Skip((pagination.PageToken - 1) * pagination.PageSize).Take(pagination.PageSize);
+
+    public static IQueryable<TSource> ApplyPagination<TSource>(this IQueryable<TSource> source, FilterPagination pagination) =>
+        source.Skip((pagination.PageToken - 1) * pagination.PageSize).Take(pagination.PageSize);
+}
---
+++ Notifications.Application/Common/Querying/Models/FilterPagination.cs
@@ -0,0 +1,8 @@
+﻿namespace Notifications.Application.Common.Querying.Models;
+
+public class FilterPagination
+{
+    public int PageToken { get; set; }
+
+    public int PageSize { get; set; }
+}
