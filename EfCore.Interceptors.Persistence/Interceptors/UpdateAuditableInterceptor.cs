262c0fe7b29e79fc1fe5612993688002824a8d39
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-23 18:34:07 +0500

    Add auditable interceptor

---
+++ EfCore.Interceptors.Persistence/Interceptors/UpdateAuditableInterceptor.cs
@@ -0,0 +1,28 @@
+﻿using EfCore.Interceptors.Domain.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace EfCore.Interceptors.Persistence.Interceptors;
+
+public class UpdateAuditableInterceptor : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData, 
+        InterceptionResult<int> result, 
+        CancellationToken cancellationToken = default
+    )
+    {
+        var auditableEntries = eventData.Context!.ChangeTracker.Entries<IAuditableEntity>().ToList();
+
+        auditableEntries.ForEach(entry =>
+        {
+            if (entry.State == EntityState.Modified)
+                entry.Property(nameof(IAuditableEntity.ModifiedTime)).CurrentValue = DateTimeOffset.UtcNow;
+
+            if (entry.State == EntityState.Added)
+                entry.Property(nameof(IAuditableEntity.CreatedTime)).CurrentValue = DateTimeOffset.UtcNow;
+        });
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+}
