0b476d4bb2de95515a41f1a5b3ab448075766e2b
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-23 13:23:52 +0500

    Create user repository

---
+++ EfCore.Interceptors.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using EfCore.Interceptors.Domain.Entities;
+using EfCore.Interceptors.Persistence.DataContexts;
+using EfCore.Interceptors.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace EfCore.Interceptors.Persistence.Repositories;
+
+public class UserRepository(IdentityDbContext dbContext) : EntityRepositoryBase<User, IdentityDbContext>(dbContext), IUserRepository
+{
+    public new IQueryable<User> Get(Expression<Func<User, bool>>? predicate = default, bool asNoTracking = false) => base.Get(predicate, asNoTracking);
+
+    public new ValueTask<User?> GetByIdAsync(Guid userId, bool asNoTracking = false, CancellationToken cancellationToken = default) =>
+        base.GetByIdAsync(userId, asNoTracking, cancellationToken);
+
+    public new ValueTask<User> CreateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default) =>
+        base.CreateAsync(user, saveChanges, cancellationToken);
+
+    public new ValueTask<User> UpdateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default) =>
+        base.UpdateAsync(user, saveChanges, cancellationToken);
+
+    public new ValueTask<User?> DeleteByIdAsync(Guid userId, bool saveChanges = true, CancellationToken cancellationToken = default) =>
+        base.DeleteByIdAsync(userId, saveChanges, cancellationToken);
+}
