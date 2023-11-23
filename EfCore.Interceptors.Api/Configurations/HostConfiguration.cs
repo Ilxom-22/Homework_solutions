c8b00543172ab288d734501a6e79a11b2c7f68bb
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-23 13:50:28 +0500

    Add users controller

--- EfCore.Interceptors.Api/Configurations/HostConfiguration.cs
+++ EfCore.Interceptors.Api/Configurations/HostConfiguration.cs
@@ -6,6 +6,7 @@ public static partial class HostConfiguration
     {
         builder
             .AddIdentityInfrastructure()
+            .AddMappers()
             .AddDevTools()
             .AddExposers();
 
