50aada5bf0284824a1f3861d8b206e64e0ddf83f
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-23 12:19:51 +0500

    Add basic host configurations and swagger dev tool

---
+++ EfCore.Interceptors.Api/Configurations/HostConfiguration.Extensions.cs
@@ -0,0 +1,35 @@
+﻿namespace EfCore.Interceptors.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddControllers();
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddDevTools(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddEndpointsApiExplorer();
+
+        return builder;
+    }
+
+    private static WebApplication UseDevTools(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    }
+
+    private static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+}
