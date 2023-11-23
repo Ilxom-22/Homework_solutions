d2ad7b1530ffc85b953c7b2a4521d0a1babd1d4d
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-23 18:34:24 +0500

    Add soft deletion interceptor

---
+++ EfCore.Interceptors.Persistence/Interceptors/UpdateSoftDeletionInterceptor.cs
@@ -0,0 +1,30 @@
+﻿using EfCore.Interceptors.Domain.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace EfCore.Interceptors.Persistence.Interceptors;
+
+public class UpdateSoftDeletionInterceptor : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData, 
+        InterceptionResult<int> result, 
+        CancellationToken cancellationToken = default
+    )
+    {
+        var softDeletionEntries = eventData.Context!.ChangeTracker.Entries<ISoftDeletedEntity>().ToList();
+
+        softDeletionEntries.ForEach(
+            entry =>
+            {
+                if (entry.State == EntityState.Deleted)
+                {
+                    entry.State = EntityState.Modified;
+                    entry.Property(nameof(ISoftDeletedEntity.DeletedTime)).CurrentValue = DateTimeOffset.UtcNow;
+                    entry.Property(nameof(ISoftDeletedEntity.IsDeleted)).CurrentValue = true;
+                }
+            });
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+}
