501d7a0dd877cc3c0b6fec5fe1cfcaccba870fc4
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-24 16:11:47 +0500

    Add default admin migration

--- EfCore.Interceptors.Persistence/EntityConfigurations/UserConfiguration.cs
+++ EfCore.Interceptors.Persistence/EntityConfigurations/UserConfiguration.cs
@@ -9,5 +9,14 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.Property(user => user.FirstName).IsRequired().HasMaxLength(64);
+
+        builder.HasData(
+            new User
+            {
+                Id = Guid.Parse("4CBB0A46-7023-440F-8DF6-11BCAF59FBA6"),
+                FirstName = "Admin",
+                CreatedTime = DateTime.UtcNow,
+            }
+        );
     }
 }
---
+++ EfCore.Interceptors.Persistence/Migrations/20231124104214_Add Default Admin Migration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfCore.Interceptors.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultAdminMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedTime", "DeletedByUserId", "DeletedTime", "FirstName", "IsDeleted", "ModifiedByUserId", "ModifiedTime" },
+                values: new object[] { new Guid("4cbb0a46-7023-440f-8df6-11bcaf59fba6"), new DateTimeOffset(new DateTime(2023, 11, 24, 10, 42, 13, 868, DateTimeKind.Unspecified).AddTicks(8852), new TimeSpan(0, 0, 0, 0, 0)), null, null, "Admin", false, null, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("4cbb0a46-7023-440f-8df6-11bcaf59fba6"));
+        }
+    }
+}
