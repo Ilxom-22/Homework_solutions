29c686c2e1fd5eead592d3260a09ed522f1ad77a
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-27 23:52:43 +0500

    Create Item repository

---
+++ EfCore.Interceptors.Persistence/Repositories/Interfaces/IItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using EfCore.Interceptors.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace EfCore.Interceptors.Persistence.Repositories.Interfaces;
+
+public interface IItemRepository
+{
+    IQueryable<Item> Get(Expression<Func<Item, bool>>? predicate = default, bool asNoTracking = false);
+
+    ValueTask<Item?> GetByIdAsync(Guid userId, bool asNoTracking = false, CancellationToken cancellationToken = default);
+
+    ValueTask<Item> CreateAsync(Item user, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    ValueTask<Item> UpdateAsync(Item user, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    ValueTask<Item?> DeleteByIdAsync(Guid itemId, bool saveChanges = true, CancellationToken cancellationToken = default);
+}
---
+++ EfCore.Interceptors.Persistence/Repositories/ItemRepository.cs
@@ -0,0 +1,24 @@
+﻿using EfCore.Interceptors.Domain.Entities;
+using EfCore.Interceptors.Persistence.DataContexts;
+using EfCore.Interceptors.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace EfCore.Interceptors.Persistence.Repositories;
+
+public class ItemRepository(IdentityDbContext dbContext) : EntityRepositoryBase<Item, IdentityDbContext>(dbContext), IItemRepository
+{
+    ValueTask<Item> IItemRepository.CreateAsync(Item user, bool saveChanges, CancellationToken cancellationToken)
+        => CreateAsync(user, saveChanges, cancellationToken);
+
+    ValueTask<Item?> IItemRepository.DeleteByIdAsync(Guid itemId, bool saveChanges, CancellationToken cancellationToken)
+        => DeleteByIdAsync(itemId, saveChanges, cancellationToken);
+
+    IQueryable<Item> IItemRepository.Get(Expression<Func<Item, bool>>? predicate, bool asNoTracking)
+        => Get(predicate, asNoTracking);
+
+    ValueTask<Item?> IItemRepository.GetByIdAsync(Guid userId, bool asNoTracking, CancellationToken cancellationToken)
+        => GetByIdAsync(userId, asNoTracking, cancellationToken);
+
+    ValueTask<Item> IItemRepository.UpdateAsync(Item user, bool saveChanges, CancellationToken cancellationToken)
+        => UpdateAsync(user, saveChanges, cancellationToken);
+}
