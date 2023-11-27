c7293ddd2494c358ab98c36e97e3c05dd05554f9
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-27 19:29:38 +0500

    add item entity and its configuration

---
+++ EfCore.Interceptors.Domain/Entities/Item.cs
@@ -0,0 +1,14 @@
+﻿using EfCore.Interceptors.Domain.Common.Entities;
+
+namespace EfCore.Interceptors.Domain.Entities;
+
+public class Item : AuditableEntity, IModificationAuditableEntity, ICreationAuditableEntity, IDeletionAuditableEntity
+{
+    public string Name { get; set; } = default!;
+
+    public Guid? ModifiedByUserId { get; set; }
+
+    public Guid? CreatedByUserId { get; set; }
+
+    public Guid? DeletedByUserId { get; set; }
+}
---
+++ EfCore.Interceptors.Persistence/EntityConfigurations/ItemConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using EfCore.Interceptors.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EfCore.Interceptors.Persistence.EntityConfigurations;
+
+public class ItemConfiguration : IEntityTypeConfiguration<Item>
+{
+    public void Configure(EntityTypeBuilder<Item> builder)
+    {
+        builder.Property(item => item.Name).IsRequired().HasMaxLength(256);
+    }
+}
