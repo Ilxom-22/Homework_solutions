4fcc50a2173683e931f2cfc7b96cc750167f6abe
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-27 19:30:28 +0500

    Add Items migration

---
+++ EfCore.Interceptors.Persistence/Migrations/20231127142610_Items Migration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfCore.Interceptors.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemsMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    ModifiedByUserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedByUserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    DeletedByUserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DeletedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    CreatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ModifiedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("4cbb0a46-7023-440f-8df6-11bcaf59fba6"),
+                column: "CreatedTime",
+                value: new DateTimeOffset(new DateTime(2023, 11, 27, 14, 26, 9, 391, DateTimeKind.Unspecified).AddTicks(1853), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("4cbb0a46-7023-440f-8df6-11bcaf59fba6"),
+                column: "CreatedTime",
+                value: new DateTimeOffset(new DateTime(2023, 11, 27, 11, 0, 20, 457, DateTimeKind.Unspecified).AddTicks(7917), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+    }
+}
