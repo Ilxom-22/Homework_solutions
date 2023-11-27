95ec4038afafde1db1c619226ab6c549c45d12d3
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-27 23:55:40 +0500

    Add auth controller

---
+++ EfCore.Interceptors.Api/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using EfCore.Interceptors.Application.Common.Identity.Models;
+using EfCore.Interceptors.Application.Common.Identity.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EfCore.Interceptors.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController(IAuthService authService) : ControllerBase
+{
+    [HttpPost("signUp")]
+    public async ValueTask<IActionResult> SignUpAsync(UserDetails userDetails)
+        => Ok(await authService.SignUpAsync(userDetails));
+
+    [HttpPost("signIn")]
+    public IActionResult SignInAsync(UserDetails userDetails)
+        => Ok(authService.SignInAsync(userDetails));
+}
